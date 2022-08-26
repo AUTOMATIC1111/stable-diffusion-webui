a7fa62bbfe330869f24a8a6e4071fce8ea8080df
hlky <106811348+hlky@users.noreply.github.com>
2022-08-26 21:48:14 +0100

    Revert to os.listdir()

--- webui.py
+++ webui.py
@@ -566,11 +566,11 @@ def process_images(
                     sanitized_prompt = sanitized_prompt[:128] #200 is too long
                     sample_path_i = os.path.join(sample_path, sanitized_prompt)
                     os.makedirs(sample_path_i, exist_ok=True)
-                    base_count = len(glob.glob(f"{sample_path_i}/*.png"))
+                    base_count = len(os.listdir(sample_path_i))
                     filename = f"{base_count:05}-{seeds[i]}"
                 else:
                     sample_path_i = sample_path
-                    base_count = len(glob.glob(f"{sample_path_i}/*.png"))
+                    base_count = len(os.listdir(sample_path_i))
                     sanitized_prompt = sanitized_prompt
                     filename = f"{base_count:05}-{seeds[i]}_{sanitized_prompt}"[:128] #same as before
                 if not skip_save:
