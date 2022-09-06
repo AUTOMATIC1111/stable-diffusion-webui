1ae5ab7306b6970bf38be7e958489144a5c7590a
AUTOMATIC <16777216c@gmail.com>
2022-09-06 10:38:02 +0300

    some extra changes for collab compatibility

--- modules/paths.py
+++ modules/paths.py
@@ -6,7 +6,13 @@ script_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, script_path)
 
 # use current directory as SD dir if it has related files, otherwise parent dir of script as stated in guide
-sd_path = os.path.abspath('.') if os.path.exists('./ldm/models/diffusion/ddpm.py') else os.path.dirname(script_path)
+sd_path = None
+possible_sd_paths = ['.', os.path.dirname(script_path), os.path.join(script_path, 'repositories/stable-diffusion')]
+for possible_sd_path in possible_sd_paths:
+    if os.path.exists(os.path.join(possible_sd_path, 'ldm/models/diffusion/ddpm.py')):
+        sd_path = os.path.abspath(possible_sd_path)
+
+assert sd_path is not None, "Couldn't find Stable Diffusion in any of: " + possible_sd_paths
 
 # add parent directory to path; this is where Stable diffusion repo should be
 path_dirs = [
