b01db6a4994905f72de45ca33a118d468ed93bb4
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-09 21:42:46 +0200

    Cleaner condition

--- modules/images.py
+++ modules/images.py
@@ -133,10 +133,7 @@ def draw_grid_annotations(im, width, height, hor_texts, ver_texts):
 
     fontsize = (width + height) // 25
     line_spacing = fontsize // 2
-    if not opts.font:
-        fnt = ImageFont.truetype(Roboto, fontsize)
-    else:
-        fnt = ImageFont.truetype(opts.font, fontsize)
+    fnt = ImageFont.truetype(opts.font or Roboto, fontsize)
     color_active = (0, 0, 0)
     color_inactive = (153, 153, 153)
 
