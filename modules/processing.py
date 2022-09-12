df52268ba2495094021aa8cb12d75b5c7831cee5
AUTOMATIC <16777216c@gmail.com>
2022-09-12 15:41:30 +0300

    [BUG] Not Working As Intended - create a directory with name derived from the prompt #306

--- modules/processing.py
+++ modules/processing.py
@@ -275,7 +275,7 @@ def process_images(p: StableDiffusionProcessing) -> Processed:
                     image = image.convert('RGB')
 
                 if opts.samples_save and not p.do_not_save_samples:
-                    images.save_image(image, p.outpath_samples, "", seeds[i], prompts[i], opts.samples_format, info=infotext(n, i), process_info = Processed(p, output_images, all_seeds[0], infotext()))
+                    images.save_image(image, p.outpath_samples, "", seeds[i], prompts[i], opts.samples_format, info=infotext(n, i), p=p)
 
                 output_images.append(image)
 
@@ -291,7 +291,7 @@ def process_images(p: StableDiffusionProcessing) -> Processed:
                 output_images.insert(0, grid)
 
             if opts.grid_save:
-                images.save_image(grid, p.outpath_grids, "grid", all_seeds[0], all_prompts[0], opts.grid_format, info=infotext(), short_filename=not opts.grid_extended_filename)
+                images.save_image(grid, p.outpath_grids, "grid", all_seeds[0], all_prompts[0], opts.grid_format, info=infotext(), short_filename=not opts.grid_extended_filename, p=p)
 
     devices.torch_gc()
     return Processed(p, output_images, all_seeds[0], infotext())
