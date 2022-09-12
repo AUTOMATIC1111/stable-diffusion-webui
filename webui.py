97397a8db60492202a5c7d95e69d8d566661c727
AUTOMATIC <16777216c@gmail.com>
2022-09-12 16:52:06 +0300

    aaaaaaaaaaaaaaaaa

--- webui.py
+++ webui.py
@@ -49,7 +49,7 @@ def load_model_from_config(config, ckpt, verbose=False):
     if len(u) > 0 and verbose:
         print("unexpected keys:")
         print(u)
-    if cmd_opts.opts_channelslast:
+    if cmd_opts.opt_channelslast:
         model = model.to(memory_format=torch.channels_last)
     model.eval()
     return model
