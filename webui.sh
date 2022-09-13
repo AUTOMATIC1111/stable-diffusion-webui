3dd7e68e074853fd402f90728cb47ba719444c0e
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-13 17:48:10 +0200

    fix on torch_command + tested on debian

--- webui.sh
+++ webui.sh
@@ -41,6 +41,12 @@ then
     venv_dir="venv"
 fi
 
+# install command for torch
+if [[ -z "${TORCH_COMMAND}" ]]
+then
+    export TORCH_COMMAND=(python3 -m pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113)
+fi
+
 # Do not reinstall existing pip packages on Debian/Ubuntu
 export PIP_IGNORE_INSTALLED=0
 
