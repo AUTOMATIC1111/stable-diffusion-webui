c02d2fb6c30bdae5ac794f7ce772d7608ce6a56b
AUTOMATIC <16777216c@gmail.com>
2022-09-17 16:28:19 +0300

    added user.css support

--- modules/ui.py
+++ modules/ui.py
@@ -801,6 +801,11 @@ def create_ui(txt2img, img2img, run_extras, run_pnginfo):
     with open(os.path.join(script_path, "style.css"), "r", encoding="utf8") as file:
         css = file.read()
 
+    if os.path.exists(os.path.join(script_path, "style.css")):
+        with open(os.path.join(script_path, "user.css"), "r", encoding="utf8") as file:
+            usercss = file.read()
+            css += usercss
+
     if not cmd_opts.no_progressbar_hiding:
         css += css_hide_progressbar
 
