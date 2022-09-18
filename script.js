31e9d7aaf9745ca49bfb8462f6d96905d0c960f2
AUTOMATIC <16777216c@gmail.com>
2022-09-18 08:41:08 +0300

    script.js split work - dragdrop.js

--- script.js
+++ script.js
@@ -202,63 +202,6 @@ function submit(){
     return res
 }
 
-function isValidImageList( files ) {
-    return files && files?.length === 1 && ['image/png', 'image/gif', 'image/jpeg'].includes(files[0].type);
-}
-
-function dropReplaceImage( imgWrap, files ) {
-    if ( ! isValidImageList( files ) ) {
-        return;
-    }
-
-    imgWrap.querySelector('.modify-upload button + button')?.click();
-    window.requestAnimationFrame( () => {
-        const fileInput = imgWrap.querySelector('input[type="file"]');
-        if ( fileInput ) {
-            fileInput.files = files;
-            fileInput.dispatchEvent(new Event('change'));   
-        }
-    });
-}
-
-window.document.addEventListener('dragover', e => {
-    const target = e.composedPath()[0];
-    const imgWrap = target.closest('[data-testid="image"]');
-    if ( !imgWrap ) {
-        return;
-    }
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
-});
-
-window.document.addEventListener('drop', e => {
-    const target = e.composedPath()[0];
-    const imgWrap = target.closest('[data-testid="image"]');
-    if ( !imgWrap ) {
-        return;
-    }
-    e.stopPropagation();
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    dropReplaceImage( imgWrap, files );
-});
-
-window.addEventListener('paste', e => {
-    const files = e.clipboardData.files;
-    if ( ! isValidImageList( files ) ) {
-        return;
-    }
-    [...gradioApp().querySelectorAll('input[type=file][accept="image/x-png,image/gif,image/jpeg"]')]
-        .filter(input => !input.matches('.\\!hidden input[type=file]'))
-        .forEach(input => {
-            input.files = files;
-            input.dispatchEvent(new Event('change'))
-        });
-    [...gradioApp().querySelectorAll('[data-testid="image"]')]
-        .forEach(imgWrap => dropReplaceImage( imgWrap, files ));
-});
-
 function ask_for_style_name(_, prompt_text, negative_prompt_text) {
     name_ = prompt('Style name:')
     return name_ === null ? [null, null, null]: [name_, prompt_text, negative_prompt_text]
