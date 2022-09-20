c1f51aff4b02a25afa39f5c365d34cf95ffa0ecd
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-20 15:20:45 +0200

    Remove default git variables to prevent issues

--- webui-user.sh
+++ webui-user.sh
@@ -28,13 +28,13 @@ export COMMANDLINE_ARGS=""
 #export REQS_FILE="requirements_versions.txt"
 
 # Fixed git repos
-#export K_DIFFUSION_PACKAGE="git+https://github.com/crowsonkb/k-diffusion.git@1a0703dfb7d24d8806267c3e7ccc4caf67fd1331"
-#export GFPGAN_PACKAGE="git+https://github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379"
+#export K_DIFFUSION_PACKAGE=""
+#export GFPGAN_PACKAGE=""
 
 # Fixed git commits
-#export STABLE_DIFFUSION_COMMIT_HASH="69ae4b35e0a0f6ee1af8bb9a5d0016ccb27e36dc"
-#export TAMING_TRANSFORMERS_COMMIT_HASH="24268930bf1dce879235a7fddd0b2355b84d7ea6"
-#export CODEFORMER_COMMIT_HASH="c5b4593074ba6214284d6acd5f1719b6c5d739af"
-#export BLIP_COMMIT_HASH="48211a1594f1321b00f14c9f7a5b4813144b2fb9"
+#export STABLE_DIFFUSION_COMMIT_HASH=""
+#export TAMING_TRANSFORMERS_COMMIT_HASH=""
+#export CODEFORMER_COMMIT_HASH=""
+#export BLIP_COMMIT_HASH=""
 
 ###########################################
