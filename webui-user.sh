378b1e880b23dc2633c9e303c9d12a67f735787e
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-20 14:28:20 +0200

    Comment variables with default values

--- webui-user.sh
+++ webui-user.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
-###########################################
-# Change the variables below to your need:#
-###########################################
+#########################################################
+# Uncomment and change the variables below to your need:#
+#########################################################
 
 # Install directory without trailing slash
-install_dir="/home/$(whoami)"
+#install_dir="/home/$(whoami)"
 
-# Name of the subdirectory (defaults to stable-diffusion-webui)
-clone_dir="stable-diffusion-webui"
+# Name of the subdirectory
+#clone_dir="stable-diffusion-webui"
 
-# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS=(--medvram --opt-split-attention)
+# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
 export COMMANDLINE_ARGS=""
 
 # python3 executable
-python_cmd="python3"
+#python_cmd="python3"
 
 # git executable
-#export GIT=""
+#export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-venv_dir="venv"
+#venv_dir="venv"
 
 # install command for torch
-export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
-#export REQS_FILE=""
+#export REQS_FILE="requirements_versions.txt"
 
 # Fixed git repos
-#export K_DIFFUSION_PACKAGE=""
-#export GFPGAN_PACKAGE=""
+#export K_DIFFUSION_PACKAGE="git+https://github.com/crowsonkb/k-diffusion.git@1a0703dfb7d24d8806267c3e7ccc4caf67fd1331"
+#export GFPGAN_PACKAGE="git+https://github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379"
 
 # Fixed git commits
-#export STABLE_DIFFUSION_COMMIT_HASH=""
-#export TAMING_TRANSFORMERS_COMMIT_HASH=""
-#export CODEFORMER_COMMIT_HASH=""
-#export BLIP_COMMIT_HASH=""
+#export STABLE_DIFFUSION_COMMIT_HASH="69ae4b35e0a0f6ee1af8bb9a5d0016ccb27e36dc"
+#export TAMING_TRANSFORMERS_COMMIT_HASH="24268930bf1dce879235a7fddd0b2355b84d7ea6"
+#export CODEFORMER_COMMIT_HASH="c5b4593074ba6214284d6acd5f1719b6c5d739af"
+#export BLIP_COMMIT_HASH="48211a1594f1321b00f14c9f7a5b4813144b2fb9"
 
 ###########################################
