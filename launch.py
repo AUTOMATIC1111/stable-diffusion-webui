1aacb20a1f9177cea6b45f78f9f77fcc4f4f3fbb
d8ahazard <d8ahazard@gmail.com>
2022-09-20 16:46:12 -0500

    Ensure ldsr repo is up to date on launch?

--- launch.py
+++ launch.py
@@ -110,9 +110,8 @@ git_clone("https://github.com/CompVis/stable-diffusion.git", repo_dir('stable-di
 git_clone("https://github.com/CompVis/taming-transformers.git", repo_dir('taming-transformers'), "Taming Transformers", taming_transformers_commit_hash)
 git_clone("https://github.com/sczhou/CodeFormer.git", repo_dir('CodeFormer'), "CodeFormer", codeformer_commit_hash)
 git_clone("https://github.com/salesforce/BLIP.git", repo_dir('BLIP'), "BLIP", blip_commit_hash)
-
-if not os.path.isdir(repo_dir('latent-diffusion')):
-    git_clone("https://github.com/Hafiidz/latent-diffusion", repo_dir('latent-diffusion'), "LDSR", ldsr_commit_hash)
+# Using my repo until my changes are merged, as this makes interfacing with our version of SD-web a lot easier
+git_clone("https://github.com/d8ahazard/latent-diffusion", repo_dir('latent-diffusion'), "LDSR", ldsr_commit_hash)
 
 if not is_installed("lpips"):
     run_pip(f"install -r {os.path.join(repo_dir('CodeFormer'), 'requirements.txt')}", "requirements for CodeFormer")
