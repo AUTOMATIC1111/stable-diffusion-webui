1e208af1f87df370deb9b0838e2284ae18cc8bb1
Vetchems <vetrofuse@gmail.com>
2022-09-23 23:42:45 +0100

    added model hash to discord output

--- modules/discordbot.py
+++ modules/discordbot.py
@@ -200,19 +200,19 @@ async def die(ctx):
     await ctx.message.delete()
     await bot.close()
 
-async def image_send(filename,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height):
+async def image_send(filename,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height,modelhash):
         channel = bot.get_channel(post_id)
         view=Buttons()
         print(f"Uploading: {filename}")
-        await channel.send(f"Prompt: `{prompt}`\nSeed: `{seed}` - Subseed: `{subseed}` - Var Amount: `{seedvar}`\nSampler: `{sampler_name}` - Steps: `{steps}` - CFG Scale: `{cfg_scale}`\nImage Dimensions: `{width}x{height}`", file=discord.File(filename), view=view)
+        await channel.send(f"Prompt: `{prompt}`\nSeed: `{seed}` - Subseed: `{subseed}` - Var Amount: `{seedvar}`\nSampler: `{sampler_name}` - Steps: `{steps}` - CFG Scale: `{cfg_scale}`\nImage Dimensions: `{width}x{height}`\nModel Hash: `{modelhash}`", file=discord.File(filename), view=view)
 
 ##########################
 #save_sample part
-def post_result(image,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height):
+def post_result(image,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height,modelhash):
     if bot_config['main']['post_result'] == True:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        bot.loop.create_task(image_send(image,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height))
+        bot.loop.create_task(image_send(image,prompt,seed,subseed,seedvar,sampler_name,steps,cfg_scale,width,height,modelhash))
         loop.close()
 ######################
 #
