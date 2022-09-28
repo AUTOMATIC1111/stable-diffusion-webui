d645606f77ed6e09f5d985c149f50c4bcf130355
William Moorehouse <moorehousew@gmail.com>
2022-09-28 16:28:45 -0400

    Respect --ckpt-dir command-line argument when merging models (fixes #1224)

--- modules/extras.py
+++ modules/extras.py
@@ -7,7 +7,7 @@ import torch
 import tqdm
 
 from modules import processing, shared, images, devices
-from modules.shared import opts
+from modules.shared import opts, cmd_opts
 import modules.gfpgan_model
 from modules.ui import plaintext_to_html
 import modules.codeformer_model
@@ -156,17 +156,17 @@ def run_modelmerger(primary_model_name, secondary_model_name, interp_method, int
         alpha = 0.5 - math.sin(math.asin(1.0 - 2.0 * alpha) / 3.0)
         return theta0 + ((theta1 - theta0) * alpha)
 
-    if os.path.exists(primary_model_name):
-        primary_model_filename = primary_model_name
-        primary_model_name = os.path.splitext(os.path.basename(primary_model_name))[0]
-    else:
-        primary_model_filename = 'models/' + primary_model_name + '.ckpt'
+    primary_model_filename = os.path.join(cmd_opts.ckpt_dir, primary_model_name + '.ckpt')
+    if not os.path.exists(primary_model_filename):
+        primary_model_filename = os.path.join('models/', primary_model_name + '.ckpt')
+        
+    primary_model_name = os.path.splitext(os.path.basename(primary_model_filename))[0]
 
-    if os.path.exists(secondary_model_name):
-        secondary_model_filename = secondary_model_name
-        secondary_model_name = os.path.splitext(os.path.basename(secondary_model_name))[0]
-    else:
-        secondary_model_filename = 'models/' + secondary_model_name + '.ckpt'
+    secondary_model_filename = os.path.join(cmd_opts.ckpt_dir, secondary_model_name + '.ckpt')
+    if not os.path.exists(secondary_model_filename):
+        secondary_model_filename = os.path.join('models/', secondary_model_name + '.ckpt')
+        
+    secondary_model_name = os.path.splitext(os.path.basename(secondary_model_filename))[0]
 
     print(f"Loading {primary_model_filename}...")
     primary_model = torch.load(primary_model_filename, map_location='cpu')
@@ -192,8 +192,16 @@ def run_modelmerger(primary_model_name, secondary_model_name, interp_method, int
     for key in theta_1.keys():
         if 'model' in key and key not in theta_0:
             theta_0[key] = theta_1[key]
-
-    output_modelname = 'models/' + primary_model_name + '_' + str(round(interp_amount,2)) + '-' + secondary_model_name + '_' + str(round((float(1.0) - interp_amount),2)) + '-' + interp_method.replace(" ", "_") + '-merged.ckpt'
+    
+    # Respect --ckpt-dir command-line argument
+    output_modelname = ''
+    if os.path.exists(cmd_opts.ckpt_dir):
+        output_modelname = cmd_opts.ckpt_dir
+    else:
+        output_modelname = 'models/'
+    
+    output_modelname = os.path.join(output_modelname, primary_model_name + '_' + str(round(interp_amount,2)) + '-' + secondary_model_name + '_' + str(round((float(1.0) - interp_amount),2)) + '-' + interp_method.replace(" ", "_") + '-merged.ckpt')
+    
     print(f"Saving to {output_modelname}...")
     torch.save(primary_model, output_modelname)
 
