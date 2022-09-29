b4bbf6ff63152086aa15563ce8568fd7712680e8
Alex <georglegato@online.de>
2022-09-29 21:52:29 +0200

    using potrace w/o .exe

--- modules/shared.py
+++ modules/shared.py
@@ -143,7 +143,7 @@ options_templates.update(options_section(('saving-images', "Saving images/grids"
     "export_for_4chan": OptionInfo(True, "If PNG image is larger than 4MB or any dimension is larger than 4000, downscale and save copy as JPG"),
 
     "use_original_name_batch": OptionInfo(False, "Use original name for output filename during batch process in extras tab"),
-    "potrace_path" : OptionInfo("potrace.exe", "Path on your system to find the potrace command")
+    "potrace_path" : OptionInfo("potrace", "Path on your system to find the potrace command if not in your PATH-env")
 }))
 
 options_templates.update(options_section(('saving-paths', "Paths for saving"), {
