cf141157e7b49b0b3a6e57dc7aa0d1345158b4c8
RnDMonkey <ryandrutledge@gmail.com>
2022-09-30 22:02:29 -0700

    Added X/Y plot parameters to extra_generation_params

--- scripts/xy_grid.py
+++ scripts/xy_grid.py
@@ -244,6 +244,14 @@ class Script(scripts.Script):
 
             return process_images(pc)
 
+        if not x_opt.label == 'Nothing':
+            p.extra_generation_params["X/Y Plot X Type"] = x_opt.label
+            p.extra_generation_params["X Values"] = '{' + ", ".join([f'{x}' for x in xs]) + '}'
+
+        if not y_opt.label == 'Nothing':
+            p.extra_generation_params["X/Y Plot Y Type"] = y_opt.label
+            p.extra_generation_params["Y Values"] = '{' + ", ".join([f'{y}' for y in ys]) + '}'
+
         processed = draw_xy_grid(
             p,
             xs=xs,
