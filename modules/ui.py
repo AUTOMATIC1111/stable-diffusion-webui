9aa40df03059afb427bdb0076f3d318a84a29207
Vetchems <vetrofuse@gmail.com>
2022-10-02 21:28:11 +0100

    merge error fix

--- modules/ui.py
+++ modules/ui.py
@@ -853,7 +853,7 @@ def create_ui(wrap_gradio_gpu_call):
                 (denoising_strength, "Denoising strength"),
             ]
             modules.generation_parameters_copypaste.connect_paste(paste, img2img_paste_fields, img2img_prompt)
-<<<<<<< HEAD
+
     with gr.Blocks(analytics_enabled=False) as promptgen_interface:
         with gr.Row():
             genprompt = gr.Textbox(label="Prompt", elem_id="prompt", show_label=False, placeholder="Prompt", lines=4)
@@ -1260,9 +1260,9 @@ def create_ui(wrap_gradio_gpu_call):
                 genprompt,
             ]
         )
-=======
+
             token_button.click(fn=update_token_counter, inputs=[img2img_prompt, steps], outputs=[token_counter])
->>>>>>> upstream/master
+
 
     with gr.Blocks(analytics_enabled=False) as extras_interface:
         with gr.Row().style(equal_height=False):
