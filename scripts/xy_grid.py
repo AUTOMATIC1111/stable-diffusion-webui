fe6e2362e8fa5d739de6997ab155a26686d20a49
RnDMonkey <RnDMonkey@users.noreply.github.com>
2022-10-02 22:04:28 -0700

    Update xy_grid.py
    Changed XY Plot infotext value keys to not be so generic.

--- scripts/xy_grid.py
+++ scripts/xy_grid.py
@@ -246,15 +246,15 @@ class Script(scripts.Script):
 
         if not x_opt.label == 'Nothing':
             p.extra_generation_params["XY Plot X Type"] = x_opt.label
-            p.extra_generation_params["X Values"] = '{' + x_values + '}'
+            p.extra_generation_params["XY Plot X Values"] = '{' + x_values + '}'
             if x_opt.label in ["Seed","Var. seed"] and not no_fixed_seeds:
-                p.extra_generation_params["Fixed X Values"] = '{' + ", ".join([str(x) for x in xs])+ '}'
+                p.extra_generation_params["XY Plot Fixed X Values"] = '{' + ", ".join([str(x) for x in xs])+ '}'
 
         if not y_opt.label == 'Nothing':
             p.extra_generation_params["XY Plot Y Type"] = y_opt.label
-            p.extra_generation_params["Y Values"] = '{' + y_values + '}'
+            p.extra_generation_params["XY Plot Y Values"] = '{' + y_values + '}'
             if y_opt.label in ["Seed","Var. seed"] and not no_fixed_seeds:
-                p.extra_generation_params["Fixed Y Values"] = '{' + ", ".join([str(y) for y in ys])+ '}'
+                p.extra_generation_params["XY Plot Fixed Y Values"] = '{' + ", ".join([str(y) for y in ys])+ '}'
 
         processed = draw_xy_grid(
             p,
