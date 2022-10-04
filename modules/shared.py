cb3466959c55347e6fdedc7500706402b1069efd
Milly <milly.ca@gmail.com>
2022-10-05 01:19:50 +0900

    Added `grid_save_to_dirs_for_ui` option

--- modules/shared.py
+++ modules/shared.py
@@ -178,6 +178,7 @@ options_templates.update(options_section(('saving-to-dirs', "Saving to a directo
     "save_to_dirs": OptionInfo(False, "Save images to a subdirectory"),
     "grid_save_to_dirs": OptionInfo(False, "Save grids to a subdirectory"),
     "use_save_to_dirs_for_ui": OptionInfo(False, "When using \"Save\" button, save images to a subdirectory"),
+    "grid_save_to_dirs_for_ui": OptionInfo(False, "When using \"Save\" button, save grids to a subdirectory"),
     "directories_filename_pattern": OptionInfo("", "Directory name pattern"),
     "directories_max_prompt_words": OptionInfo(8, "Max prompt words for [prompt_words] pattern", gr.Slider, {"minimum": 1, "maximum": 20, "step": 1}),
 }))
