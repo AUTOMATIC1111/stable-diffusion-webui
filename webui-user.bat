d606046b91da8a78f13f754df1b495b45cff02f2
natemac <nateofs@gmail.com>
2022-10-05 09:21:57 -0500

    Update webui-user.bat
    Asks to update stable diffusion, defaults NO after 5 seconds.

--- webui-user.bat
+++ webui-user.bat
@@ -5,4 +5,14 @@ set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
+echo AUTOMATIC1111 / stable-diffusion-webui
+CHOICE /T 5 /M "Do you want to check for Updates? Yes[y] or No[n]?" /D N
+IF ERRORLEVEL 2 (GOTO :launch)
+    
+:update
+echo Checking for Updates...
+git pull https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+
+:launch
+cls
 call webui.bat
