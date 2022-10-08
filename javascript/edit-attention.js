6ccff78ae2d7248993c13564f02f1e6738b450f3
Justin Maier <Zipp425@gmail.com>
2022-10-07 22:05:05 -0600

    fix/finish edit attention

--- javascript/edit-attention.js
+++ javascript/edit-attention.js
@@ -1,41 +1,51 @@
-addEventListener('keydown', (event) => {
-	let target = event.originalTarget;
-	if (!target.hasAttribute("placeholder")) return;
-	if (!target.placeholder.toLowerCase().includes("prompt")) return;
+onUiUpdate(function(){
+	if (!txt2img_textarea) {
+		txt2img_textarea = gradioApp().querySelector("#txt2img_prompt > label > textarea");
+		txt2img_textarea?.addEventListener("keydown", (event) => edit_attention(txt2img_textarea, event));
+	}
+	if (!img2img_textarea) {
+		img2img_textarea = gradioApp().querySelector("#img2img_prompt > label > textarea");
+		img2img_textarea?.addEventListener("keydown", (event) => edit_attention(img2img_textarea, event));
+	}
+})
+
+function edit_attention(el, event) {
+	const {key, ctrlKey} = event;
+	if (!ctrlKey) return;
 
 	let plus = "ArrowUp"
 	let minus = "ArrowDown"
-	if (event.key != plus && event.key != minus) return;
+	if (key != plus && key != minus) return;
 
-	selectionStart = target.selectionStart;
-	selectionEnd = target.selectionEnd;
+	selectionStart = el.selectionStart;
+	selectionEnd = el.selectionEnd;
 	if(selectionStart == selectionEnd) return;
 
 	event.preventDefault();
 
-	if (selectionStart == 0 || target.value[selectionStart - 1] != "(") {
-		target.value = target.value.slice(0, selectionStart) +
-			"(" + target.value.slice(selectionStart, selectionEnd) + ":1.0)" +
-			target.value.slice(selectionEnd);
+	if (selectionStart == 0 || el.value[selectionStart - 1] != "(") {
+		el.value = el.value.slice(0, selectionStart) +
+			"(" + el.value.slice(selectionStart, selectionEnd) + ":1.0)" +
+			el.value.slice(selectionEnd);
 
-		target.focus();
-		target.selectionStart = selectionStart + 1;
-		target.selectionEnd = selectionEnd + 1;
+		el.focus();
+		el.selectionStart = selectionStart + 1;
+		el.selectionEnd = selectionEnd + 1;
 
 	} else {
-		end = target.value.slice(selectionEnd + 1).indexOf(")") + 1;
-		weight = parseFloat(target.value.slice(selectionEnd + 1, selectionEnd + 1 + end));
-		if (event.key == minus) weight -= 0.1;
-		if (event.key == plus) weight += 0.1;
+		end = el.value.slice(selectionEnd + 1).indexOf(")") + 1;
+		weight = parseFloat(el.value.slice(selectionEnd + 1, selectionEnd + 1 + end));
+		if (key == minus) weight -= 0.1;
+		if (key == plus) weight += 0.1;
 
 		weight = parseFloat(weight.toPrecision(12));
 
-		target.value = target.value.slice(0, selectionEnd + 1) +
+		el.value = el.value.slice(0, selectionEnd + 1) +
 			weight +
-			target.value.slice(selectionEnd + 1 + end - 1);
+			el.value.slice(selectionEnd + 1 + end - 1);
 
-		target.focus();
-		target.selectionStart = selectionStart;
-		target.selectionEnd = selectionEnd;
+		el.focus();
+		el.selectionStart = selectionStart;
+		el.selectionEnd = selectionEnd;
 	}
-});
+}
