f048b6dfe34811d1f070d6cd0e69370b9f29a285
AUTOMATIC <16777216c@gmail.com>
2022-10-09 11:59:41 +0300

    hide filenames for save button by default

--- modules/ui.py
+++ modules/ui.py
@@ -162,7 +162,7 @@ def save_files(js_data, images, do_make_zip, index):
                     zip_file.writestr(filenames[i], f.read())
         fullfns.insert(0, zip_filepath)
 
-    return fullfns, '', '', plaintext_to_html(f"Saved: {filenames[0]}")
+    return gr.File.update(value=fullfns, visible=True), '', '', plaintext_to_html(f"Saved: {filenames[0]}")
 
 
 def wrap_gradio_call(func, extra_outputs=None):
@@ -553,7 +553,7 @@ def create_ui(wrap_gradio_gpu_call):
                         do_make_zip = gr.Checkbox(label="Make Zip when Save?", value=False)
                     
                     with gr.Row():
-                        download_files = gr.File(None, file_count="multiple", interactive=False, show_label=False)
+                        download_files = gr.File(None, file_count="multiple", interactive=False, show_label=False, visible=False)
 
                 with gr.Group():
                     html_info = gr.HTML()
@@ -741,7 +741,7 @@ def create_ui(wrap_gradio_gpu_call):
                         do_make_zip = gr.Checkbox(label="Make Zip when Save?", value=False)
                     
                     with gr.Row():
-                        download_files = gr.File(None, file_count="multiple", interactive=False, show_label=False)
+                        download_files = gr.File(None, file_count="multiple", interactive=False, show_label=False, visible=False)
 
                 with gr.Group():
                     html_info = gr.HTML()
