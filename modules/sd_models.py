727e4d108674dc2813507e2a973a733ef21e8d53
AUTOMATIC <16777216c@gmail.com>
2022-10-10 20:46:55 +0300

    no to different messages plus fix using != to compare to None

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -152,15 +152,12 @@ def load_model_weights(model, checkpoint_info):
     devices.dtype_vae = torch.float32 if shared.cmd_opts.no_half or shared.cmd_opts.no_half_vae else torch.float16
 
     vae_file = os.path.splitext(checkpoint_file)[0] + ".vae.pt"
-    if os.path.exists(vae_file):
-        print(f"Found VAE Weights: {vae_file}")
-    elif shared.cmd_opts.vae_path != None:
+
+    if not os.path.exists(vae_file) and shared.cmd_opts.vae_path is not None:
         vae_file = shared.cmd_opts.vae_path
-        print(f'No VAE found for inside the model folder. Using CLI specified : {vae_file}')
-    else:
-        print("No VAE found for inside the model folder. Passing.")
 
     if os.path.exists(vae_file):
+        print(f"Loading VAE weights from: {vae_file}")
         vae_ckpt = torch.load(vae_file, map_location="cpu")
         vae_dict = {k: v for k, v in vae_ckpt["state_dict"].items() if k[0:4] != "loss"}
 
