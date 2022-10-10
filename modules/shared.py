f98338faa84ecce503e68d8ba13d5f7bbae52730
AUTOMATIC <16777216c@gmail.com>
2022-10-10 23:15:48 +0300

    add an option to not add watermark to created images

--- modules/shared.py
+++ modules/shared.py
@@ -173,6 +173,7 @@ options_templates.update(options_section(('saving-images', "Saving images/grids"
 
     "use_original_name_batch": OptionInfo(False, "Use original name for output filename during batch process in extras tab"),
     "save_selected_only": OptionInfo(True, "When using 'Save' button, only save a single selected image"),
+    "do_not_add_watermark": OptionInfo(False, "Do not add watermark to images"),
 }))
 
 options_templates.update(options_section(('saving-paths', "Paths for saving"), {
