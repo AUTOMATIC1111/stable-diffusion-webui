0afb675a19b50634d5562b740f0a02e854b765b2
dsw676676 <dsw676676@gmail.com>
2022-10-11 14:11:19 +0800

    2022/10/11

---
+++ tag.py
@@ -0,0 +1,3 @@
+a = "1girl armpit_crease backlighting bag bangs black_bow blurry blurry_background blush_visible_through_hair border bow breasts collarbone cowboy_shot floating_hair food gradient_hair green_eyes hair_between_eyes hair_bow hand_up handbag holding holding_food ice_cream ice_cream_cone lens_flare light_blush long_hair looking_at_food medium_breasts multicolored_hair no_bra off-shoulder_shirt off_shoulder open_mouth outdoors outside_border pink_hair plaid plaid_skirt pool poolside raised_eyebrows see-through shiny shiny_hair shirt sidelocks skirt sky smile solo strap translucent_hair twintails very_long_hair white_border white_hair"
+
+print(",".join(a.split(" ")))
---
+++ webui-user-显存小于等于4g使用.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=--lowvram --precision full --no-half
+
+call webui.bat
