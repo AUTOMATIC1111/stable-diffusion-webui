7b1db45e1fda8603d4617affd976066be5e5b821
yfszzx <yfszzx@gmail.com>
2022-10-11 20:17:27 +0800

    images history improvement

--- javascript/images_history.js
+++ javascript/images_history.js
@@ -1,66 +1,124 @@
-function init_images_history(){ 
-	if (gradioApp().getElementById('txt2img_images_history_first_page') == null) {
-		setTimeout(init_images_history, 1000)
-	} else {
-		tab_list = ["txt2img", "img2img"]
-		for (i in tab_list){
-			tab = tab_list[i]
-			gradioApp().getElementById(tab + "_images_history_first_page").click()
-			$(gradioApp().getElementById(tab + '_images_history')).addClass("images_history_gallery")
-			item = $(gradioApp().getElementById(tab + '_images_history_set_index'))
-			item.addClass("images_history_set_index")
-			item.hide()
-		}		
-	}
-	
+images_history_tab_list = ["txt2img", "img2img", "extras"]
+function images_history_init(){ 
+    if (gradioApp().getElementById('txt2img_images_history_first_page') == null) {
+        setTimeout(images_history_init, 500)
+    } else {        
+        for (i in images_history_tab_list ){
+            tab = images_history_tab_list[i]
+            gradioApp().getElementById(tab + '_images_history').classList.add("images_history_gallery")
+            gradioApp().getElementById(tab + '_images_history_set_index').classList.add("images_history_set_index")      
+            
+        }
+        gradioApp().getElementById("txt2img_images_history_first_page").click()     
+    }    
 }
-setTimeout(init_images_history, 1000)
+setTimeout(images_history_init, 500)
+var images_history_button_actions = function(){
+    if (!this.classList.contains("transform")){        
+        gallery = this.parentElement
+        while(!gallery.classList.contains("images_history_gallery")){gallery = gallery.parentElement}        
+        buttons = gallery.querySelectorAll(".gallery-item")
+        i = 0
+        hidden_list = []
+        buttons.forEach(function(e){
+            if (e.style.display == "none"){
+                hidden_list.push(i)
+            }
+            i += 1
+        })
+        if (hidden_list.length > 0){
+            setTimeout(images_history_hide_buttons, 10, hidden_list, gallery)
+        }
+        
+    }    
+    images_history_set_image_info(this) 
+
+}
 onUiUpdate(function(){
-    fullImg_preview = gradioApp().querySelectorAll('#txt2img_images_history img.w-full')
-        if(fullImg_preview.length > 0){
-	    	fullImg_preview.forEach(set_history_index_from_img);
-    }
-    fullImg_preview = gradioApp().querySelectorAll('#img2img_images_history img.w-full')
-        if(fullImg_preview.length > 0){
-	    	fullImg_preview.forEach(set_history_index_from_img);
+    for (i in images_history_tab_list ){
+        tab = images_history_tab_list[i]
+        buttons = gradioApp().querySelectorAll('#' + tab + '_images_history .gallery-item')
+        buttons.forEach(function(bnt){    
+            bnt.addEventListener('click', images_history_button_actions, true)
+         });
     }
 })
-
-function set_history_gallery_index(item){
-	buttons = item.find(".gallery-item")
-	// alert(item.attr("id") + " " + buttons.length)
-	index = -1
-	i = 0
-	buttons.each(function(){
-		if($(this).hasClass("!ring-2")){ index = i }
-		i += 1
-	})
-	if (index == -1){
-		setTimeout(set_history_gallery_index, 10, item)     
-	} else {
-		item = item.find(".images_history_set_index").first()
-    	item.attr("img_index", index)
-    	item.click()
-	}
-}
-function set_history_index_from_img(e){
-    if(e && e.parentElement.tagName == 'BUTTON'){
-    	bnt = $(e).parent()
-    	if (bnt.hasClass("transform")){
-    		bnt.off("click").on("click",function(){
-    		set_history_gallery_index($(this).parents(".images_history_gallery").first())  
-     		})
-    	} else {
-    		bnt.off("mousedown").on("mousedown", function(){
-    		set_history_gallery_index($(this).parents(".images_history_gallery").first())  
-    		})
-
-		}  		
-    }
-}
-function images_history_get_current_img(is_image2image, image_path, files){
-	head = is_image2image?"img2img":"txt2img"
-	s =  $(gradioApp().getElementById(head + '_images_history_set_index')).attr("img_index")
-	return [s, image_path, files]
+function images_history_hide_buttons(hidden_list, gallery){
+    buttons = gallery.querySelectorAll(".gallery-item")
+    num = 0
+    buttons.forEach(function(e){
+        if (e.style.display == "none"){
+            num += 1
+        }
+    })
+    if (num == hidden_list.length){
+        setTimeout(images_history_hide_buttons, 10, hidden_list, gallery)
+    } 
+    for( i in hidden_list){
+        buttons[hidden_list[i]].style.display = "none"
+    }    
 }
 
+function images_history_set_image_info(button){
+    item = button.parentElement
+    while(item.tagName != "DIV"){item = item.parentElement}    
+    buttons = item.querySelectorAll(".gallery-item")
+    index = -1
+    i = 0
+    buttons.forEach(function(e){
+        if(e==button){index = i}
+        if(e.style.display != "none"){
+            i += 1
+        }        
+    })
+    gallery = button.parentElement
+    while(!gallery.classList.contains("images_history_gallery")){gallery = gallery.parentElement}    
+    set_btn = gallery.querySelector(".images_history_set_index")
+    set_btn.setAttribute("img_index", index)
+    set_btn.click()
+}
+
+function images_history_get_current_img(tabname, image_path, files){
+    s =  gradioApp().getElementById(tabname + '_images_history_set_index').getAttribute("img_index")
+    return [s, image_path, files]
+}
+
+function images_history_delete(tabname, img_path, img_file_name, page_index, filenames, image_index){
+    image_index = parseInt(image_index)
+    tab = gradioApp().getElementById(tabname + '_images_history')
+    set_btn = tab.querySelector(".images_history_set_index")
+    buttons = []
+    tab.querySelectorAll(".gallery-item").forEach(function(e){
+        if (e.style.display != 'none'){
+            buttons.push(e)
+        }
+    })
+
+    
+    img_num = buttons.length / 2
+    if (img_num == 1){
+            setTimeout(function(tabname){
+                gradioApp().getElementById(tabname + '_images_history_renew_page').click()
+            }, 30, tabname) 
+    } else {
+        buttons[image_index].style.display = 'none'
+        buttons[image_index + img_num].style.display = 'none'
+        if (image_index  >= img_num - 1){
+            console.log(buttons.length, img_num)
+            btn = buttons[img_num - 2]
+        } else {
+            btn = buttons[image_index + 1]           
+        }    
+        setTimeout(function(btn){btn.click()}, 30, btn)
+    }  
+    
+    return [tabname, img_path, img_file_name, page_index, filenames, image_index]
+}
+
+function images_history_turnpage(img_path, page_index, image_index, tabname){
+    buttons = gradioApp().getElementById(tabname + '_images_history').querySelectorAll(".gallery-item")
+    buttons.forEach(function(elem) {
+         elem.style.display = 'block'
+    })
+    return [img_path, page_index, image_index, tabname]
+}
--- modules/images_history.py
+++ modules/images_history.py
@@ -1,117 +1,130 @@
 import os
 def get_recent_images(dir_name, page_index, step, image_index):
-	print(image_index)
-	page_index = int(page_index)
-	f_list = os.listdir(dir_name)
-	file_list = []
-	for file in f_list:
-		if file[-4:] == ".txt":
-			continue
-		file_list.append(file)	
-	file_list = sorted(file_list, key=lambda file: -os.path.getctime(os.path.join(dir_name, file)))
-	num = 48
-	max_page_index = len(file_list) // num + 1	
-	page_index = max_page_index if page_index == -1 else page_index + step
-	page_index = 1 if page_index < 1 else page_index	
-	page_index = max_page_index if page_index > max_page_index else page_index
-	idx_frm = (page_index - 1) * num
-	file_list = file_list[idx_frm:idx_frm + num]
-	print(f"Loading history page {page_index}")	
-	image_index = int(image_index)
-	if image_index < 0 or image_index > len(file_list)  - 1:
-		current_file = None 
-		hide_image = None
-	else:
-		current_file =  file_list[int(image_index)]
-		hide_image = os.path.join(dir_name, current_file)
-	return [os.path.join(dir_name, file) for file in file_list], page_index, file_list, current_file, hide_image
-def first_page_click(dir_name, page_index, image_index):
-	return get_recent_images(dir_name, 1, 0, image_index)
-def end_page_click(dir_name, page_index, image_index):
-	return get_recent_images(dir_name, -1, 0, image_index)
-def prev_page_click(dir_name, page_index, image_index):
-	return get_recent_images(dir_name, page_index, -1, image_index)
-def next_page_click(dir_name, page_index, image_index):	
-	return get_recent_images(dir_name, page_index, 1, image_index)
-def page_index_change(dir_name, page_index, image_index):	
-	return get_recent_images(dir_name, page_index, 0, image_index)
+    #print(image_index)
+    page_index = int(page_index)
+    f_list = os.listdir(dir_name)
+    file_list = []
+    for file in f_list:
+        if file[-4:] == ".txt":
+            continue
+        file_list.append(file)    
+    file_list = sorted(file_list, key=lambda file: -os.path.getctime(os.path.join(dir_name, file)))
+    num = 48
+    max_page_index = len(file_list) // num + 1    
+    page_index = max_page_index if page_index == -1 else page_index + step
+    page_index = 1 if page_index < 1 else page_index    
+    page_index = max_page_index if page_index > max_page_index else page_index
+    idx_frm = (page_index - 1) * num
+    file_list = file_list[idx_frm:idx_frm + num]
+    #print(f"Loading history page {page_index}")    
+    image_index = int(image_index)
+    if image_index < 0 or image_index > len(file_list)  - 1:
+        current_file = None 
+        hide_image = None
+    else:
+        current_file =  file_list[int(image_index)]
+        hide_image = os.path.join(dir_name, current_file)
+    return [os.path.join(dir_name, file) for file in file_list], page_index, file_list, current_file, hide_image
+def first_page_click(dir_name, page_index, image_index, tabname):
+    return get_recent_images(dir_name, 1, 0, image_index)
+def end_page_click(dir_name, page_index, image_index, tabname):
+    return get_recent_images(dir_name, -1, 0, image_index)
+def prev_page_click(dir_name, page_index, image_index, tabname):
+    return get_recent_images(dir_name, page_index, -1, image_index)
+def next_page_click(dir_name, page_index, image_index, tabname):    
+    return get_recent_images(dir_name, page_index, 1, image_index)
+def page_index_change(dir_name, page_index, image_index, tabname):    
+    return get_recent_images(dir_name, page_index, 0, image_index)
 
 def show_image_info(num, image_path, filenames):
-	file = filenames[int(num)]
-	return file, num, os.path.join(image_path, file)
-def delete_image(is_img2img, dir_name, name, page_index, filenames, image_index):
-	print("filename", name)
-	path = os.path.join(dir_name, name)		
-	if os.path.exists(path):
-		print(f"Delete file {path}")
-		os.remove(path)	
-	images, page_index, file_list, current_file, hide_image = get_recent_images(dir_name, page_index, 0, image_index)
-	return images, page_index, file_list, current_file, hide_image
+    #print("set img",num)
+    file = filenames[int(num)]
+    return file, num, os.path.join(image_path, file)
+def delete_image(tabname, dir_name, name, page_index, filenames, image_index):
+    #print("filename", name)
+    path = os.path.join(dir_name, name)           
+    if os.path.exists(path):
+        print(f"Delete file {path}")
+        os.remove(path)    
+        new_file_list = []
+        for f in filenames:
+            if f == name:
+                continue
+            new_file_list.append(f)
+    else:
+        print(f"Not exists file {path}")
+        new_file_list = filenames
+    return page_index, new_file_list
 
+def show_images_history(gr, opts, tabname, run_pnginfo, switch_dict):
+        if tabname == "txt2img":
+            dir_name = opts.outdir_txt2img_samples
+        elif tabname == "img2img":
+            dir_name = opts.outdir_img2img_samples
+        elif tabname == "extras":
+            dir_name = opts.outdir_extras_samples
+        with gr.Row():    
+                renew_page = gr.Button('Renew', elem_id=tabname + "_images_history_renew_page") 
+                first_page = gr.Button('First', elem_id=tabname + "_images_history_first_page")
+                prev_page = gr.Button('Prev') 
+                page_index = gr.Number(value=1, label="Page Index")
+                next_page = gr.Button('Next', elem_id=tabname + "_images_history_next_page") 
+                end_page = gr.Button('End')    
+        with gr.Row(elem_id=tabname + "_images_history"):            
+            with gr.Row():     
+                with gr.Column():
+                    history_gallery = gr.Gallery(show_label=False).style(grid=6)
+                with gr.Column():     
+                    with gr.Row():          
+                        delete = gr.Button('Delete')
+                        pnginfo_send_to_txt2img = gr.Button('Send to txt2img')
+                        pnginfo_send_to_img2img = gr.Button('Send to img2img')
+                    with gr.Row():
+                        with gr.Column():
+                            img_file_info = gr.Textbox(label="Generate Info")
+                            img_file_name = gr.Textbox(label="File Name")    
+                    with gr.Row():    
+                            # hiden items
+                            img_path = gr.Textbox(dir_name, visible=False)                
+                            tabname_box = gr.Textbox(tabname, visible=False)    
+                            image_index = gr.Textbox(value=-1, visible=False)                        
+                            set_index = gr.Button('set_index',  elem_id=tabname + "_images_history_set_index", visible=False)
+                            filenames = gr.State()
+                            hide_image = gr.Image(visible=False, type="pil")
+                            info1 = gr.Textbox(visible=False)
+                            info2 = gr.Textbox(visible=False)
 
-def show_images_history(gr, opts, is_img2img, run_pnginfo, switch_dict):
-		def id_name(is_img2img, name):	
-			return ("img2img" if is_img2img else "txt2img") + "_" + name
-		if is_img2img:
-			dir_name = opts.outdir_img2img_samples
-		else:
-			dir_name = opts.outdir_txt2img_samples
-		with gr.Row():	 
-				first_page = gr.Button('First', elem_id=id_name(is_img2img,"images_history_first_page"))
-				prev_page = gr.Button('Prev') 
-				page_index = gr.Number(value=1, label="Page Index")
-				next_page = gr.Button('Next') 
-				end_page = gr.Button('End')	
-		with gr.Row(elem_id=id_name(is_img2img,"images_history")):			
-			with gr.Row():	 
-				with gr.Column():
-					history_gallery = gr.Gallery(show_label=False).style(grid=6)
-				with gr.Column():	 
-					with gr.Row():	      
-						delete = gr.Button('Delete')
-						pnginfo_send_to_txt2img = gr.Button('Send to txt2img')
-						pnginfo_send_to_img2img = gr.Button('Send to img2img')
-					with gr.Row():
-						with gr.Column():
-							img_file_info = gr.Textbox(dir_name, label="Generate Info")
-							img_file_name = gr.Textbox(label="File Name")	
-					with gr.Row():	
-							# hiden items
-							img_path = gr.Textbox(dir_name, visible=False)				
-							is_img2img_flag = gr.Checkbox(is_img2img, visible=False)	
-							image_index = gr.Textbox(value=-1, visible=False)						
-							set_index = gr.Button('set_index',  elem_id=id_name(is_img2img,"images_history_set_index"))
-							filenames = gr.State()
-							hide_image = gr.Image(visible=False, type="pil")
-							info1 = gr.Textbox(visible=False)
-							info2 = gr.Textbox(visible=False)
+                
+        # turn pages
+        gallery_inputs = [img_path, page_index, image_index, tabname_box]
+        gallery_outputs = [history_gallery, page_index, filenames, img_file_name, hide_image]
 
-				
-		# turn pages
-		gallery_inputs = [img_path, page_index, image_index]
-		gallery_outputs = [history_gallery, page_index, filenames, img_file_name, hide_image]
-		first_page.click(first_page_click, inputs=gallery_inputs, outputs=gallery_outputs)
-		next_page.click(next_page_click, inputs=gallery_inputs, outputs=gallery_outputs)
-		prev_page.click(prev_page_click, inputs=gallery_inputs, outputs=gallery_outputs)		
-		end_page.click(end_page_click, inputs=gallery_inputs, outputs=gallery_outputs)
-		page_index.submit(page_index_change, inputs=gallery_inputs, outputs=gallery_outputs)
-		#page_index.change(page_index_change, inputs=[is_img2img_flag, img_path,  page_index], outputs=[history_gallery, page_index])
+        first_page.click(first_page_click, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)
+        next_page.click(next_page_click, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)
+        prev_page.click(prev_page_click, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)        
+        end_page.click(end_page_click, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)
+        page_index.submit(page_index_change, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)
+        renew_page.click(page_index_change, _js="images_history_turnpage", inputs=gallery_inputs, outputs=gallery_outputs)
+        #page_index.change(page_index_change, inputs=[tabname_box, img_path,  page_index], outputs=[history_gallery, page_index])
 
-		#other funcitons
-		set_index.click(show_image_info, _js="images_history_get_current_img", inputs=[is_img2img_flag,  img_path, filenames], outputs=[img_file_name, image_index, hide_image])
-		delete.click(delete_image, inputs=[is_img2img_flag, img_path, img_file_name, page_index, filenames, image_index], outputs=gallery_outputs) 
-		hide_image.change(fn=run_pnginfo, inputs=[hide_image], outputs=[info1, img_file_info, info2])
-		switch_dict["fn"](pnginfo_send_to_txt2img, switch_dict["t2i"], img_file_info, 'switch_to_txt2img')
-		switch_dict["fn"](pnginfo_send_to_img2img, switch_dict["i2i"], img_file_info, 'switch_to_img2img_img2img')
-	
-		
+        #other funcitons
+        set_index.click(show_image_info, _js="images_history_get_current_img", inputs=[tabname_box,  img_path, filenames], outputs=[img_file_name, image_index, hide_image])
+        delete.click(delete_image,_js="images_history_delete", inputs=[tabname_box, img_path, img_file_name, page_index, filenames, image_index], outputs=[page_index, filenames]) 
+        hide_image.change(fn=run_pnginfo, inputs=[hide_image], outputs=[info1, img_file_info, info2])
+        switch_dict["fn"](pnginfo_send_to_txt2img, switch_dict["t2i"], img_file_info, 'switch_to_txt2img')
+        switch_dict["fn"](pnginfo_send_to_img2img, switch_dict["i2i"], img_file_info, 'switch_to_img2img_img2img')
+    
+        
 def create_history_tabs(gr, opts, run_pnginfo, switch_dict):
-	with gr.Blocks(analytics_enabled=False) as images_history:
-		with gr.Tabs() as tabs:			
-			with gr.Tab("txt2img history", id="images_history_txt2img"):
-				with gr.Blocks(analytics_enabled=False) as images_history_txt2img:   
-					show_images_history(gr, opts, False, run_pnginfo, switch_dict) 
-			with gr.Tab("img2img history", id="images_history_img2img"):
-				with gr.Blocks(analytics_enabled=False) as images_history_img2img:
-					show_images_history(gr, opts, True, run_pnginfo, switch_dict)
-	return images_history
+    with gr.Blocks(analytics_enabled=False) as images_history:
+        with gr.Tabs() as tabs:            
+            with gr.Tab("txt2img history"):
+                with gr.Blocks(analytics_enabled=False) as images_history_txt2img:   
+                    show_images_history(gr, opts, "txt2img", run_pnginfo, switch_dict) 
+            with gr.Tab("img2img history"):
+                with gr.Blocks(analytics_enabled=False) as images_history_img2img:
+                    show_images_history(gr, opts, "img2img", run_pnginfo, switch_dict)
+            with gr.Tab("extras history"):
+                with gr.Blocks(analytics_enabled=False) as images_history_img2img:
+                    show_images_history(gr, opts, "extras", run_pnginfo, switch_dict)
+    return images_history
