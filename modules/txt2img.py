3728024f6dfe498b662b3b34508c15780cf3a423
Buckzor <bucklogos@yahoo.co.uk>
2022-10-13 17:07:06 +0100

    Added first_pass_width and height as adjustable inputs to "High Res Fix"

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -6,7 +6,7 @@ import modules.processing as processing
 from modules.ui import plaintext_to_html
 
 
-def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, scale_latent: bool, denoising_strength: float, *args):
+def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, scale_latent: bool, denoising_strength: float, first_pass_width: int, first_pass_height: int, *args):
     p = StableDiffusionProcessingTxt2Img(
         sd_model=shared.sd_model,
         outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
@@ -32,6 +32,9 @@ def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2:
         enable_hr=enable_hr,
         scale_latent=scale_latent if enable_hr else None,
         denoising_strength=denoising_strength if enable_hr else None,
+		first_pass_width=first_pass_width if enable_hr else None,
+		first_pass_height=first_pass_height if enable_hr else None,
+
     )
 
     if cmd_opts.enable_console_prompts:
