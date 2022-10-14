b7a5ab26db0fb34117f2ea4ff3c36ee7f9fbee55
Zeithrold <zeithrold@gmail.com>
2022-10-14 13:50:44 +0800

    moved repository url to changeable environment variable

--- webui-user.sh
+++ webui-user.sh
@@ -30,13 +30,25 @@ export COMMANDLINE_ARGS=""
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
 
-# Fixed git repos
+# Fixed git-based pip packages
+# Example: "git+https://github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379"
 #export K_DIFFUSION_PACKAGE=""
 #export GFPGAN_PACKAGE=""
+#export DEEPDANBOORU_PACKAGE=""
+
+# Fixed git repos
+# Example: "https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/c/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl"
+#export STABLE_DIFFUSION_REPO="https://github.com/CompVis/stable-diffusion.git"
+#export TAMING_REANSFORMERS_REPO="https://github.com/CompVis/taming-transformers.git"
+#export K_DIFFUSION_REPO="https://github.com/crowsonkb/k-diffusion.git"
+#export CODEFORMET_REPO="https://github.com/sczhou/CodeFormer.git"
+#export BLIP_REPO="https://github.com/salesforce/BLIP.git"
 
 # Fixed git commits
+# Example: "69ae4b35e0a0f6ee1af8bb9a5d0016ccb27e36dc"
 #export STABLE_DIFFUSION_COMMIT_HASH=""
 #export TAMING_TRANSFORMERS_COMMIT_HASH=""
+#export K_DIFFUSION_COMMIT_HASH=""
 #export CODEFORMER_COMMIT_HASH=""
 #export BLIP_COMMIT_HASH=""
 
