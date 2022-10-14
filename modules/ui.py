f6431820e42bbc9ace00c43d053938f0615a07ea
AUTOMATIC <16777216c@gmail.com>
2022-10-14 23:17:28 +0300

    disabling history - i knew it was slow as fuck but i didn't realize it would also show galleries on launch

--- modules/ui.py
+++ modules/ui.py
@@ -1089,7 +1089,8 @@ def create_ui(wrap_gradio_gpu_call):
         "t2i":txt2img_paste_fields,
         "i2i":img2img_paste_fields
     }
-    images_history = img_his.create_history_tabs(gr, opts, wrap_gradio_call(modules.extras.run_pnginfo), images_history_switch_dict) 
+
+    #images_history = img_his.create_history_tabs(gr, opts, wrap_gradio_call(modules.extras.run_pnginfo), images_history_switch_dict)
 
     with gr.Blocks() as modelmerger_interface:
         with gr.Row().style(equal_height=False):
@@ -1486,7 +1487,7 @@ Requested path was: {f}
         (img2img_interface, "img2img", "img2img"),
         (extras_interface, "Extras", "extras"),
         (pnginfo_interface, "PNG Info", "pnginfo"),
-        (images_history, "History", "images_history"),
+        #(images_history, "History", "images_history"),
         (modelmerger_interface, "Checkpoint Merger", "modelmerger"),
         (train_interface, "Train", "ti"),
         (settings_interface, "Settings", "settings"),
