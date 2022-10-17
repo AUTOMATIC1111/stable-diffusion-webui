b99d3cf6dd9bc817e51d0d0a6e8eb12c7c0ac6af
AUTOMATIC <16777216c@gmail.com>
2022-10-17 08:41:02 +0300

    make CLIP interrogate ranks output sane values

--- modules/interrogate.py
+++ modules/interrogate.py
@@ -123,7 +123,7 @@ class InterrogateModels:
 
         return caption[0]
 
-    def interrogate(self, pil_image, include_ranks=False):
+    def interrogate(self, pil_image):
         res = None
 
         try:
@@ -156,8 +156,8 @@ class InterrogateModels:
                 for name, topn, items in self.categories:
                     matches = self.rank(image_features, items, top_count=topn)
                     for match, score in matches:
-                        if include_ranks:
-                            res += f", ({match}:{score})"
+                        if shared.opts.interrogate_return_ranks:
+                            res += f", ({match}:{score/100:.3f})"
                         else:
                             res += ", " + match
 
