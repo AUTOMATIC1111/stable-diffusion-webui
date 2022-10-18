ad90ff6c8c815a9dda6f2618275aff8bd303c0e6
PencilMario <sir.p@foxmail.com>
2022-10-18 22:17:43 +0800

    🐛 开修！

--- modules/sptools.py
+++ modules/sptools.py
@@ -76,6 +76,8 @@ def save_outcsv(filename):
         new_data = pandas.read_csv(filename)
         if not os.path.exists(logdir.format(model_name, short_filename)):
             shutil.copy(filename, logdir.format(model_name, short_filename))
+            new_data.drop(1)
+            new_data.to_csv(filename)
             print("\n已经保存loss的log文件："+filename)
         else:
             old_data=pandas.read_csv(logdir.format(model_name, short_filename))
@@ -83,7 +85,7 @@ def save_outcsv(filename):
             merged = pandas.concat(frames, ignore_index=True)
             os.remove(logdir.format(model_name, short_filename))
             merged.to_csv(logdir.format(model_name, short_filename))
-            new_data.drop(new_data.index[1:99])
-            new_data.to_csv(filename, mode='w')
+            new_data.drop(1)
+            new_data.to_csv(filename)
             print("\n已经合并loss的log文件: "+filename)
     wrap_gradio_gpu_call(copy_csv(filename), extra_outputs=None)
