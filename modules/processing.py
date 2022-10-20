aacac7b0d2585ae40eb98401d5418c3011fd39f0
w-e-w <40751091+w-e-w@users.noreply.github.com>
2022-10-20 14:48:50 +0900

    fix img2img color correction only applying to the first image in a batch
    for batch size > 1

--- modules/processing.py
+++ modules/processing.py
@@ -440,10 +440,10 @@ def process_images(p: StableDiffusionProcessing) -> Processed:
 
                 image = Image.fromarray(x_sample)
 
-                if p.color_corrections is not None and i < len(p.color_corrections):
+                if p.color_corrections is not None and i // p.batch_size < len(p.color_corrections):
                     if opts.save and not p.do_not_save_samples and opts.save_images_before_color_correction:
                         images.save_image(image, p.outpath_samples, "", seeds[i], prompts[i], opts.samples_format, info=infotext(n, i), p=p, suffix="-before-color-correction")
-                    image = apply_color_correction(p.color_corrections[i], image)
+                    image = apply_color_correction(p.color_corrections[i // p.batch_size], image)
 
                 if p.overlay_images is not None and i < len(p.overlay_images):
                     overlay = p.overlay_images[i]
