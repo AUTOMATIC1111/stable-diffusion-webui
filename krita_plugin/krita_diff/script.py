e2d8238c591c6ef0e79bdd7132af1af1f88c88c9
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-10-21 09:10:50 +0800

    a bit more documentation

--- krita_plugin/krita_diff/script.py
+++ krita_plugin/krita_diff/script.py
@@ -1,6 +1,16 @@
 import os
 
-from krita import Krita, QByteArray, QImage, QObject, QTimer
+from krita import (
+    Application,
+    Document,
+    Krita,
+    Node,
+    QByteArray,
+    QImage,
+    QObject,
+    QTimer,
+    Selection,
+)
 
 from .client import Client
 from .config import Config
@@ -22,6 +32,29 @@ from .utils import create_layer, find_optimal_selection_region
 # Does it actually have to be a QObject?
 # The only possible use I see is for event emitting
 class Script(QObject):
+    cfg: Config
+    """config singleton"""
+    client: Client
+    """API client singleton"""
+    status: str
+    """Current status (shown in status bar)"""
+    app: Application
+    """Krita's Application instance (KDE Application)"""
+    doc: Document
+    """Currently opened document if any"""
+    node: Node
+    """Currently selected layer in Krita"""
+    selection: Selection
+    """Selection region in Krita"""
+    x: int
+    """Left position of selection"""
+    y: int
+    """Top position of selection"""
+    width: int
+    """Width of selection"""
+    height: int
+    """Height of selection"""
+
     def __init__(self):
         # Persistent settings (should reload between Krita sessions)
         self.cfg = Config()
@@ -32,19 +65,23 @@ class Script(QObject):
         self.status = STATE_INIT
 
     def restore_defaults(self, if_empty=False):
+        """Restore to default config."""
         self.cfg.restore_defaults(not if_empty)
 
         if not if_empty:
             self.set_status(STATE_RESET_DEFAULT)
 
     def set_status_callback(self, cb):
+        """Used by GUI to provide callback for setting the status bar message."""
         self._status_cb = cb
 
     def set_status(self, state):
+        """Change the satus, setting the status bar message in the process."""
         self.status = state
         self._status_cb(state)
 
     def update_selection(self):
+        """Update references to key Krita objects as well as selection information."""
         self.app = Krita.instance()
         self.doc = self.app.activeDocument()
         # self.doc doesnt exist at app startup
@@ -69,6 +106,7 @@ class Script(QObject):
                 self.height = self.selection.height()
 
     def adjust_selection(self):
+        """Adjust selection region to account for scaling and striding to prevent image stretch."""
         if self.selection is not None and self.cfg("fix_aspect_ratio", bool):
             x, y, width, height = find_optimal_selection_region(
                 self.cfg("sd_base_size", int),
@@ -87,6 +125,7 @@ class Script(QObject):
             self.height = height
 
     def update_config(self):
+        """Update certain config/state from the backend."""
         return self.client.get_config()
 
     def save_img(self, path, is_mask=False):
