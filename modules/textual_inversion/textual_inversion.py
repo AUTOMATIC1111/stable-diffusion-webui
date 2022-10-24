3926d87cbb771ad3615fb5b733da5c2c76c85e7a
ccchan49 <60914289+ccchan49@users.noreply.github.com>
2022-10-23 23:44:13 -0700

    Fixing another duplicate line

--- modules/textual_inversion/textual_inversion.py
+++ modules/textual_inversion/textual_inversion.py
@@ -211,7 +211,6 @@ def statistics(data):
         std = stdev(data)
     total_information = f"loss:{mean(data):.3f}" + u"\u00B1" + f"({std/ (len(data) ** 0.5):.3f})"
     recent_data = data[-32:]
-    recent_information = f"recent 32 loss:{mean(recent_data):.3f}"+u"\u00B1"+f"({stdev(recent_data)/ (len(recent_data)**0.5):.3f})"
     if len(recent_data) < 2:
         std = 0
     else:
