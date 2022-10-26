ca87e10328ef375c9d104458a408e2c8a4552a08
timntorres <timothynarcisotorres@gmail.com>
2022-10-26 13:09:08 -0700

    Save hypernet settings to text file a la PR #3601

--- modules/hypernetworks/hypernetwork.py
+++ modules/hypernetworks/hypernetwork.py
@@ -325,7 +325,20 @@ def report_statistics(loss_info:dict):
         except Exception as e:
             print(e)
 
-
+def save_settings_to_file(num_of_dataset_images, h, hypernetwork_name, learn_rate, batch_size, data_root, log_directory, training_width, training_height, steps, create_image_every, save_hypernetwork_every, template_file, preview_from_txt2img, preview_prompt, preview_negative_prompt, preview_steps, preview_sampler_index, preview_cfg_scale, preview_seed, preview_width, preview_height):
+    sd_checkpoint = h.sd_checkpoint
+    sd_checkpoint_name = h.sd_checkpoint_name
+    layer_structure = h.layer_structure
+    activation_func = h.activation_func
+    weight_init = h.weight_init
+    add_layer_norm = h.add_layer_norm
+    use_dropout = h.use_dropout
+    names = ('num_of_dataset_images', 'sd_checkpoint', 'sd_checkpoint_name', 'layer_structure', 'activation_func', 'weight_init', 'add_layer_norm', 'use_dropout', 'hypernetwork_name', 'learn_rate', 'batch_size', 'data_root', 'log_directory', 'training_width', 'training_height', 'steps', 'create_image_every', 'save_hypernetwork_every', 'template_file', 'preview_from_txt2img', 'preview_prompt', 'preview_negative_prompt', 'preview_steps', 'preview_sampler_index', 'preview_cfg_scale', 'preview_seed', 'preview_width', 'preview_height')
+    hypernet_settings_str = "datetime: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+    for n in names:
+        hypernet_settings_str += f'{n}: {str(eval(n))}\n'
+    with open(os.path.join(log_directory, 'settings.txt'), 'a+') as fout:
+        fout.write(hypernet_settings_str + '\n\n')
 
 def train_hypernetwork(hypernetwork_name, learn_rate, batch_size, data_root, log_directory, training_width, training_height, steps, create_image_every, save_hypernetwork_every, template_file, preview_from_txt2img, preview_prompt, preview_negative_prompt, preview_steps, preview_sampler_index, preview_cfg_scale, preview_seed, preview_width, preview_height):
     # images allows training previews to have infotext. Importing it at the top causes a circular import problem.
@@ -368,6 +381,7 @@ def train_hypernetwork(hypernetwork_name, learn_rate, batch_size, data_root, log
     checkpoint = sd_models.select_checkpoint()
     hypernetwork.sd_checkpoint = checkpoint.hash
     hypernetwork.sd_checkpoint_name = checkpoint.model_name
+    save_settings_to_file(len(ds), hypernetwork, hypernetwork_name, learn_rate, batch_size, data_root, log_directory, training_width, training_height, steps, create_image_every, save_hypernetwork_every, template_file, preview_from_txt2img, preview_prompt, preview_negative_prompt, preview_steps, preview_sampler_index, preview_cfg_scale, preview_seed, preview_width, preview_height)
 
     weights = hypernetwork.weights()
     for weight in weights:
