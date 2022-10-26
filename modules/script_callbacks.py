0dd8480281ffa3e58439a3ce059c02d9f3baa5c7
MMaker <mmaker@mmaker.moe>
2022-10-26 11:08:44 -0400

    fix: Correct before image saved callback

--- modules/script_callbacks.py
+++ modules/script_callbacks.py
@@ -69,7 +69,7 @@ def ui_settings_callback():
 
 
 def before_image_saved_callback(params: ImageSaveParams):
-    for c in callbacks_image_saved:
+    for c in callbacks_before_image_saved:
         try:
             c.callback(params)
         except Exception:
