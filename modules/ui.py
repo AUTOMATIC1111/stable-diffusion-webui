a133042c669f666763f5da0f4440abdc839db653
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2022-10-28 10:01:46 +0700

    Forgot to remove this from train_embedding

--- modules/ui.py
+++ modules/ui.py
@@ -1409,8 +1409,6 @@ def create_ui(wrap_gradio_gpu_call):
                 training_width,
                 training_height,
                 steps,
-                clip_grad_mode,
-                clip_grad_value,
                 create_image_every,
                 save_embedding_every,
                 template_file,
