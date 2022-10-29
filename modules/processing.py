b358a5057200923ff0b5280271689bd185c3f0e4
Maurdekye <ncolaprete@gmail.com>
2022-10-29 14:01:09 -0400

    store clip skip when value = 1

--- modules/processing.py
+++ modules/processing.py
@@ -406,7 +406,7 @@ def create_infotext(p, all_prompts, all_seeds, all_subseeds, comments, iteration
         "Seed resize from": (None if p.seed_resize_from_w == 0 or p.seed_resize_from_h == 0 else f"{p.seed_resize_from_w}x{p.seed_resize_from_h}"),
         "Denoising strength": getattr(p, 'denoising_strength', None),
         "Eta": (None if p.sampler is None or p.sampler.eta == p.sampler.default_eta else p.sampler.eta),
-        "Clip skip": None if clip_skip <= 1 else clip_skip,
+        "Clip skip": clip_skip,
         "ENSD": None if opts.eta_noise_seed_delta == 0 else opts.eta_noise_seed_delta,
     }
 
