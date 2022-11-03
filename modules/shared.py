701869bea75f3950e77df4fe9705b57ff5216f8f
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2022-11-03 20:15:44 +0700

    Update shared.py

--- modules/shared.py
+++ modules/shared.py
@@ -320,7 +320,7 @@ options_templates.update(options_section(('training', "Training"), {
 options_templates.update(options_section(('sd', "Stable Diffusion"), {
     "sd_model_checkpoint": OptionInfo(None, "Stable Diffusion checkpoint", gr.Dropdown, lambda: {"choices": modules.sd_models.checkpoint_tiles()}, refresh=sd_models.list_models, onchange_refresh=["sd_vae"]),
     "sd_checkpoint_cache": OptionInfo(0, "Checkpoints to cache in RAM", gr.Slider, {"minimum": 0, "maximum": 10, "step": 1}),
-    "sd_vae": OptionInfo("auto", "SD VAE", gr.Dropdown, lambda: {"choices": list(sd_vae.vae_list)}, refresh=sd_vae.refresh_vae_list),
+    "sd_vae": OptionInfo("auto", "SD VAE", gr.Dropdown, lambda: {"choices": sd_vae.vae_list}, refresh=sd_vae.refresh_vae_list),
     "sd_vae_as_default": OptionInfo(False, "Use selected VAE as default fallback instead", onchange_refresh=["sd_vae"]),
     "sd_base_vae_cache": OptionInfo("file", "SD Base VAE Cache", gr.Dropdown, lambda: {"choices": ["ram", "file", "none"]}),
     "sd_hypernetwork": OptionInfo("None", "Hypernetwork", gr.Dropdown, lambda: {"choices": ["None"] + [x for x in hypernetworks.keys()]}, refresh=reload_hypernetworks),
