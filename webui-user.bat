1b9fbf85bc754963219e5699ce14c1ab80290a05
Liso7150 <kontakt.liso@gmail.com>
2022-11-05 21:34:01 +0100

    s

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--gradio-img2img-tool color-sketch --max-batch-count=100 --ui-config-file=ui-config.json --no-half-vae --vae-path=models/Stable-diffusion/Breast-Expansion/hyper_vae.pt --deepdanbooru
+set COMMANDLINE_ARGS=--allow-code --gradio-img2img-tool color-sketch --max-batch-count=100 --ui-config-file=ui-config.json --no-half-vae --vae-path=models/Stable-diffusion/Breast-Expansion/hyper_vae.pt --deepdanbooru
 
 call webui.bat
