cd6c55c1ab14fcab15329cde599cf79e8d555657
pepe10-gpu <pepe.dannyboy@gmail.com>
2022-11-06 17:05:51 -0800

    16xx card fix
    cudnn

--- modules/devices.py
+++ modules/devices.py
@@ -39,8 +39,11 @@ def torch_gc():
 
 def enable_tf32():
     if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.enabled = True
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.allow_tf32 = True
+
 
 
 errors.run(enable_tf32, "Enabling TF32")
