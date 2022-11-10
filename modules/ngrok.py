2505f39e28177452a92426f3b60d8edbe6ed1b14
JingShing <JingShingLai@gmail.com>
2022-11-10 20:39:20 +0800

    Add username and password in ngrok.

--- modules/ngrok.py
+++ modules/ngrok.py
@@ -1,14 +1,22 @@
 from pyngrok import ngrok, conf, exception
 
-
 def connect(token, port, region):
     if token == None:
         token = 'None'
+    else:
+        if ':' in token:
+            # token = authtoken:username:password
+            account = token.split(':')[1] + ':' + token.split(':')[-1]
+            token = token.split(':')[0]
+
     config = conf.PyngrokConfig(
         auth_token=token, region=region
     )
     try:
-        public_url = ngrok.connect(port, pyngrok_config=config).public_url
+        if account:
+            public_url = ngrok.connect(port, pyngrok_config=config, auth=account).public_url
+        else:
+            public_url = ngrok.connect(port, pyngrok_config=config).public_url
     except exception.PyngrokNgrokError:
         print(f'Invalid ngrok authtoken, ngrok connection aborted.\n'
               f'Your token: {token}, get the right one on https://dashboard.ngrok.com/get-started/your-authtoken')
