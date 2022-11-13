c7be83bf0240498d9382e2afeaa3f0677d26c7f6
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2022-11-13 11:11:14 +0700

    Misc
    Misc

--- modules/sd_vae.py
+++ modules/sd_vae.py
@@ -154,8 +154,7 @@ def load_vae(model, vae_file=None):
         if vae_opt not in vae_dict:
             vae_dict[vae_opt] = vae_file
             vae_list.append(vae_opt)
-            # shared.opts.data['sd_vae'] = vae_opt
-    else:
+    elif loaded_vae_file:
         restore_base_vae(model)
 
     loaded_vae_file = vae_file
