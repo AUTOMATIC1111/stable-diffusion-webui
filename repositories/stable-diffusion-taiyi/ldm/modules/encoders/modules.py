ce9dea3ab37344c4343e4c0e0d24a83bb38dc785
ganruyi <ganruyi@idea.edu.cn>
2022-11-19 13:51:13 +0800

    support Taiyi-Stable-Diffusion-1B-Chinese-EN-v0.1

--- repositories/stable-diffusion-taiyi/ldm/modules/encoders/modules.py
+++ repositories/stable-diffusion-taiyi/ldm/modules/encoders/modules.py
@@ -138,8 +138,8 @@ class FrozenCLIPEmbedder(AbstractEncoder):
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
     def __init__(self, version="openai/clip-vit-large-patch14", device="cuda", max_length=77):
         super().__init__()
-        self.tokenizer = CLIPTokenizer.from_pretrained(version)
-        self.transformer = CLIPTextModel.from_pretrained(version)
+        self.tokenizer = CLIPTokenizer.from_pretrained(version, subfolder='tokenizer')
+        self.transformer = CLIPTextModel.from_pretrained(version+'/text_encoder')
         self.device = device
         self.max_length = max_length
         self.freeze()
