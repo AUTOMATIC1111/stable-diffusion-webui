7193814cf7b052fe52596cab89c5f0fd95823950
wywywywy <wywywywy@gmail.com>
2022-11-29 19:22:53 +0000

    Added purpose of this hijack to comments

--- modules/sd_hijack_autoencoder.py
+++ modules/sd_hijack_autoencoder.py
@@ -1,3 +1,7 @@
+# The content of this file comes from the ldm/models/autoencoder.py file of the compvis/stable-diffusion repo
+# The VQModel & VQModelInterface were subsequently removed from ldm/models/autoencoder.py when we moved to the stability-ai/stablediffusion repo
+# As the LDSR upscaler relies on VQModel & VQModelInterface, the hijack aims to put them back into the ldm.models.autoencoder
+
 import torch
 import pytorch_lightning as pl
 import torch.nn.functional as F
