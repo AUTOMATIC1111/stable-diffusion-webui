9827526692f46fc333e5d304ddd6a58fa46b4c40
Ephil012 <Etp5501@gmail.com>
2022-12-03 18:00:51 -0500

    Add correct install directory for MacOS

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -9,5 +9,6 @@ export TORCH_COMMAND="pip install torch==1.12.1 torchvision==0.13.1"
 export K_DIFFUSION_REPO="https://github.com/brkirch/k-diffusion.git"
 export K_DIFFUSION_COMMIT_HASH="51c9778f269cedb55a4d88c79c0246d35bdadb71"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
+export install_dir="/Users/$(whoami)"
 
 ####################################################################
