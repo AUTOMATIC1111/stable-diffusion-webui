5ec8981df46ff6e678c09dd2c1bf4d873ac22a46
brkirch <brkirch@users.noreply.github.com>
2022-12-03 02:28:53 -0500

    Revert most launch.py changes, add mac user script
    Adds an addition file to read environment variables from when the webui.sh is run from macOS.

--- webui.sh
+++ webui.sh
@@ -4,6 +4,14 @@
 # change the variables in webui-user.sh instead #
 #################################################
 
+# If run from macOS, load defaults from webui-macos-env.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ -f webui-macos-env.sh ]]
+        then
+        source ./webui-macos-env.sh
+    fi
+fi
+
 # Read variables from webui-user.sh
 # shellcheck source=/dev/null
 if [[ -f webui-user.sh ]]
