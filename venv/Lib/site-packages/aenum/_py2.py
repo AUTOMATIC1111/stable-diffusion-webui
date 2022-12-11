9170224d230fb22387e955d0b40ceda116f688b6
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2022-12-11 18:06:22 -0500

    Delete venv/Lib/site-packages directory

--- venv/Lib/site-packages/aenum/_py2.py
+++
@@ -1,7 +0,0 @@
-from operator import div as _div_
-from inspect import getargspec
-
-def raise_with_traceback(exc, tb):
-    raise exc, None, tb
-
-__all__ = ['_div_', 'getargspec', 'raise_with_traceback']
