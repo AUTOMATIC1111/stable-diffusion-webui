b0fee53c43bdbda6f739926fad0dd59fb5fdfa1b
Anmol Chawla <anmol@dashtoon.com>
2022-12-16 19:15:58 +0530

    Allow webui to run as root

--- webui.sh
+++ webui.sh
@@ -56,7 +56,7 @@ then
 fi
 
 # this script cannot be run as root by default
-can_run_as_root=0
+can_run_as_root=1
 
 # read any command line flags to the webui.sh script
 while getopts "f" flag > /dev/null 2>&1
