13e0295ab682299e3280eb6ff28be0870f2bc57c
Akiba <akiba@anzu.link>
2022-12-24 11:17:21 +0800

    fix: xformers use importlib

--- modules/import_hook.py
+++ modules/import_hook.py
@@ -1,18 +1,5 @@
-import builtins
 import sys
-
-old_import = builtins.__import__
-IMPORT_BLACKLIST = []
 
 
 if "xformers" not in "".join(sys.argv):
-    IMPORT_BLACKLIST.append("xformers")
-
-
-def import_hook(*args, **kwargs):
-    if args[0] in IMPORT_BLACKLIST:
-        raise ImportError("Import of %s is blacklisted" % args[0])
-    return old_import(*args, **kwargs)
-
-
-builtins.__import__ = import_hook
+    sys.modules["xformers"] = None
