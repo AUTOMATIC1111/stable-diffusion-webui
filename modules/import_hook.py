0c747d4013f41f6c887a63d256af884aa8872f91
AUTOMATIC <16777216c@gmail.com>
2022-12-24 07:57:56 +0300

    add a comment for disable xformers hack

--- modules/import_hook.py
+++ modules/import_hook.py
@@ -1,5 +1,5 @@
 import sys
 
-
-if "xformers" not in "".join(sys.argv):
+# this will break any attempt to import xformers which will prevent stability diffusion repo from trying to use it
+if "--xformers" not in "".join(sys.argv):
     sys.modules["xformers"] = None
