de22d382f6c7cdd6536a55f6d63c8a319cd19af6
Berkin Güler <berkinguler@live.com>
2023-01-02 14:23:03 +0300

    prepare first then launch for multi gpu

--- multi_gpu_launch.sh
+++ multi_gpu_launch.sh
@@ -3,8 +3,10 @@
 gpu_count=$(nvidia-smi -L | wc -l);
 port=7860
 
+python3 launch.py --prepare-only --api --nowebui --listen "$@"
+
 for i in $(seq 0 "$gpu_count")
 do
-  python3 launch.py --api --nowebui --listen --device-id "$i" --port "$port" "$@";
+  python3 launch.py --no-prepare --api --nowebui --listen --device-id "$i" --port "$port" "$@";
   port=$((port + 1));
 done
