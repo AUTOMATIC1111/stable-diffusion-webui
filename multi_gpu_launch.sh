fae0585160926d0c849d6ed2a61a9f37c29e6004
Berkin Güler <berkinguler@live.com>
2023-01-02 23:27:20 +0300

    gpu count seq fix

--- multi_gpu_launch.sh
+++ multi_gpu_launch.sh
@@ -5,8 +5,8 @@ port=7860
 
 python3 launch.py --prepare-only --api --nowebui --listen "$@"
 
-for i in $(seq 0 "$gpu_count")
+for i in $(seq 0 $((gpu_count - 1)))
 do
-  screen -dm python3 launch.py --no-prepare --api --nowebui --listen --device-id "$i" --port "$port" "$@";
+  screen -d -m python3 launch.py --no-prepare --api --nowebui --listen --device-id "$i" --port "$port" "$@";
   port=$((port + 1));
 done
