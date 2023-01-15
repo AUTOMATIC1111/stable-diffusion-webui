fc25af3939f0366f147892a8ae5b9da56495352b
AUTOMATIC <16777216c@gmail.com>
2023-01-15 23:22:51 +0300

    remove unneeded log from progressbar

--- javascript/progressbar.js
+++ javascript/progressbar.js
@@ -139,8 +139,6 @@ function requestProgress(id_task, progressbarContainer, gallery, atEnd, onProgre
 
     var fun = function(id_task, id_live_preview){
         request("/internal/progress", {"id_task": id_task, "id_live_preview": id_live_preview}, function(res){
-            console.log(res)
-
             if(res.completed){
                 removeProgressBar()
                 return
