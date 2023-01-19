81276cde90ebecfab317cc62a0100d298c3c43c4
poiuty <poiuty@protonmail.com>
2023-01-19 16:56:45 +0300

    internal progress relative path

--- javascript/progressbar.js
+++ javascript/progressbar.js
@@ -160,7 +160,7 @@ function requestProgress(id_task, progressbarContainer, gallery, atEnd, onProgre
     }
 
     var fun = function(id_task, id_live_preview){
-        request("/internal/progress", {"id_task": id_task, "id_live_preview": id_live_preview}, function(res){
+        request("./internal/progress", {"id_task": id_task, "id_live_preview": id_live_preview}, function(res){
             if(res.completed){
                 removeProgressBar()
                 return
