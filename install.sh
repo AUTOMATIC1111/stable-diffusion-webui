d76cdc93e314df1c3ed3dddb7dd13ce7db2dcda5
Vladimir Mandic <mandic00@live.com>
2023-01-18 20:18:08 -0500

    update install script

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 python --version
 pip3 --version
 echo "installing general requirements"
-pip3 install --disable-pip-version-check --quiet --requirement requirements.txt
+pip3 install --disable-pip-version-check --quiet --no-warn-conflicts --requirement requirements.txt
 echo "installing versioned requirements"
-pip3 install --disable-pip-version-check --quiet --requirement requirements_versions.txt
+pip3 install --disable-pip-version-check --quiet --no-warn-conflicts --requirement requirements_versions.txt
