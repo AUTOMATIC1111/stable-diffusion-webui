18a09c7e0032e2e655269e8e2b4f1ca6ed0cc7d3
dan <guaneec@gmail.com>
2023-01-19 17:36:23 +0800

    Simplification and bugfix

--- modules/textual_inversion/preprocess.py
+++ modules/textual_inversion/preprocess.py
@@ -124,13 +124,11 @@ def center_crop(image: Image, w: int, h: int):
 def multicrop_pic(image: Image, mindim, maxdim, minarea, maxarea, objective, threshold):
     iw, ih = image.size
     err = lambda w, h: 1-(lambda x: x if x < 1 else 1/x)(iw/ih/(w/h))
-    try:
-        w, h = max(((w, h) for w in range(mindim, maxdim+1, 64) for h in range(mindim, maxdim+1, 64)
-            if minarea <= w * h <= maxarea and err(w, h) <= threshold),
-            key= lambda wh: ((objective=='Maximize area')*wh[0]*wh[1], -err(*wh))
-        )
-    except ValueError:
-        return
+    w, h = max(((w, h) for w in range(mindim, maxdim+1, 64) for h in range(mindim, maxdim+1, 64)
+        if minarea <= w * h <= maxarea and err(w, h) <= threshold),
+        key= lambda wh: (wh[0]*wh[1], -err(*wh))[::1 if objective=='Maximize area' else -1],
+        default=None
+    )
     return center_crop(image, w, h)
     
 
