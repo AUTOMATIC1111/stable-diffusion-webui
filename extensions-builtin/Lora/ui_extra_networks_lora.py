500d9a32c7b1f877c8f44159a9a10c594b545a80
AUTOMATIC <16777216c@gmail.com>
2023-01-21 23:11:37 +0300

    add --lora-dir commandline option

--- extensions-builtin/Lora/ui_extra_networks_lora.py
+++ extensions-builtin/Lora/ui_extra_networks_lora.py
@@ -31,5 +31,5 @@ class ExtraNetworksPageLora(ui_extra_networks.ExtraNetworksPage):
             }
 
     def allowed_directories_for_previews(self):
-        return [lora.lora_dir]
+        return [shared.cmd_opts.lora_dir]
 
