d02603e3fa2ec269246859a030dc48d387278166
Vladimir Mandic <mandic00@live.com>
2023-01-22 10:24:04 -0500

    add compile cmdflag

--- modules/shared.py
+++ modules/shared.py
@@ -101,6 +101,7 @@ parser.add_argument("--tls-keyfile", type=str, help="Partially enables TLS, requ
 parser.add_argument("--tls-certfile", type=str, help="Partially enables TLS, requires --tls-keyfile to fully function", default=None)
 parser.add_argument("--server-name", type=str, help="Sets hostname of server", default=None)
 parser.add_argument("--gradio-queue", action='store_true', help="Uses gradio queue; experimental option; breaks restart UI button")
+parser.add_argument("--compile", type=str, help="Use Torch Dynamo compile with specified backend", default=None)
 
 
 script_loading.preload_extensions(extensions.extensions_dir, parser)
