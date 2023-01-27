ada17dbd7c4c68a4e559848d2e6f2a7799722806
brkirch <brkirch@users.noreply.github.com>
2023-01-27 10:19:43 -0500

    Refactor conditional casting, fix upscalers

--- modules/devices.py
+++ modules/devices.py
@@ -83,6 +83,14 @@ dtype_unet = torch.float16
 unet_needs_upcast = False
 
 
+def cond_cast_unet(input):
+    return input.to(dtype_unet) if unet_needs_upcast else input
+
+
+def cond_cast_float(input):
+    return input.float() if unet_needs_upcast else input
+
+
 def randn(seed, shape):
     torch.manual_seed(seed)
     if device.type == 'mps':
