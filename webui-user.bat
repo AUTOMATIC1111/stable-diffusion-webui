a03f6449085ad734370db568d11823a370cf6bad
Liso7150 <85982694+Liso7150@users.noreply.github.com>
2023-02-04 01:28:32 -0800

    Update webui-user.bat
    dificult args and 2.0 migration for python 11

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set torch_command = os.environ.get('TORCH_COMMAND', "pip install --pre torch --force-reinstall --extra-index-url https://download.pytorch.org/whl/nightly/cu117")
+set COMMANDLINE_ARGS= --xformers --no-half-vae --no-half --disable-nan-check --lowvram --use-cpu esrgan, interrogate --opt-sub-quad-attention  --administrator --gradio-img2img-tool color-sketch --enable-console-prompts --api --precision full --no-half
+git pull
 call webui.bat
