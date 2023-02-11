8b3cc7f95f372170707a3316d081dc5c75cf635e
Matěj Račinský <racinsky.matej@seznam.cz>
2023-02-11 15:16:03 +0100

    added my notes

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\ProgramData\Anaconda3\envs\image_generation\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--theme dark
 
 call webui.bat
