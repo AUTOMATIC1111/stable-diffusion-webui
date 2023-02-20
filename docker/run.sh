39787299494c5001d2ac051c9e586418e1da3e78
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-02-20 12:07:26 +0100

    Improved run.sh script showing the UI URL

--- docker/run.sh
+++ docker/run.sh
@@ -8,3 +8,7 @@ else
 fi
 
 $compose up -d
+
+echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker compose ps --status running --format json | sed -e 's/.*"PublishedPort":\([0-9]\+\),.*/\1/g') \n\n"
+echo "To stop showing the logs precc CTRL-C"
+$compose logs -f
