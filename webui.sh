bc013a7342f3a6daba77a6027e910cdb7bb3880b
wangdongming <wangdongming@hifive.ai>
2023-03-04 18:59:38 +0800

    [feat](*): update

--- webui.sh
+++ webui.sh
@@ -38,7 +38,6 @@ then
     python_cmd="python3"
 fi
 
-
 # git executable
 if [[ -z "${GIT}" ]]
 then
@@ -150,7 +149,6 @@ else
     printf "Clone stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
     sudo "${GIT}" clone https://github.com/Jackstrawcd/stable-diffusion-webui.git "${clone_dir}"
-    ls "${clone_dir}"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
