705059a83a5cef24a6f3048c7ca3195fb699f3a4
wangdongming <wangdongming@hifive.ai>
2023-03-04 22:24:26 +0800

    [feat](*): update

--- launch.py
+++ launch.py
@@ -134,7 +134,7 @@ def run_pip(args, desc=None):
         return
 
     index_url_line = f' --index-url {index_url}' if index_url != '' else ''
-    return run(f'"sudo {python}" -m pip {args} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}")
+    return run(f'"{python}" -m pip {args} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}")
 
 
 def check_run_python(code):
