7f67cd7bfe75156c10826c4e7abdadb9ce51fc92
high_byte <email@hanan.beer>
2023-03-09 06:42:53 +0200

    clarify the need to explicitly specify xformers

--- modules/import_hook.py
+++ modules/import_hook.py
@@ -3,3 +3,4 @@ import sys
 # this will break any attempt to import xformers which will prevent stability diffusion repo from trying to use it
 if "--xformers" not in "".join(sys.argv):
     sys.modules["xformers"] = None
+    print('Note: not using xformers. To enable xformers install it and pass --xformers flag')
