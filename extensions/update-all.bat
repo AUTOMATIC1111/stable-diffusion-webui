b07b7057f0636aa142471ec27841a8001a85f98b
vladlearns <vladlearns@gmail.com>
2023-03-09 16:25:18 +0200

    chore: removed scripts and added a flag to launch.py

--- extensions/update-all.bat
+++
@@ -1,1 +0,0 @@
-for /d %%i in (*) do @if exist "%%i\.git" (echo Pulling updates for %%i... & git -C "%%i" pull)
--- extensions/update-all.sh
+++
@@ -1,3 +0,0 @@
-ls | while read dir; do if [ -d "$dir/.git" ]; 
-then echo "Pulling updates for $dir..."; 
-git -C "$dir" pull; fi; done
