04924241218bb51bee255bebc6c66ef1de449f4a
bluelovers <codelovers@users.sourceforge.net>
2023-03-12 10:18:33 +0800

    feat: try sort as ignore-case
    https://github.com/AUTOMATIC1111/stable-diffusion-webui/issues/8368

--- modules/textual_inversion/textual_inversion.py
+++ modules/textual_inversion/textual_inversion.py
@@ -129,7 +129,7 @@ class EmbeddingDatabase:
         if first_id not in self.ids_lookup:
             self.ids_lookup[first_id] = []
 
-        self.ids_lookup[first_id] = sorted(self.ids_lookup[first_id] + [(ids, embedding)], key=lambda x: len(x[0]), reverse=True)
+        self.ids_lookup[first_id] = sorted(self.ids_lookup[first_id] + [(ids, embedding)], key=lambda x: len(x[0]), reverse=True, cmp=lambda x, y: x.lower() > y.lower())
 
         return embedding
 
@@ -196,7 +196,7 @@ class EmbeddingDatabase:
             return
 
         for root, dirs, fns in os.walk(embdir.path, followlinks=True):
-            for fn in fns:
+            for fn in sorted(fns, key=str.lower):
                 try:
                     fullfn = os.path.join(root, fn)
 
