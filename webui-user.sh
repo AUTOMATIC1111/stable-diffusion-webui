fb68d93b6a579a424919b22682cf067ce9a8e13f
ParityError <36368048+ParityError@users.noreply.github.com>
2023-03-28 18:27:44 -0700

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -4,7 +4,7 @@
 #########################################################
 
 # Install directory without trailing slash
-#install_dir="$(pwd)"
+#install_dir="/home/$(whoami)"
 
 # Name of the subdirectory
 #clone_dir="stable-diffusion-webui"
