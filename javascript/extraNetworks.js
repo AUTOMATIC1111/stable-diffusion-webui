26d0c124442ab750ef0d2ed6b962d79dd4e64eb3
Miao Xiang <one111eric@hotmail.com>
2023-04-01 18:54:36 -0700

    Revert "load description to input box by clicking on card(except checkpoint)"
    This reverts commit bd8d3f169206003f7ab50118e2188c99644ec86a.

--- javascript/extraNetworks.js
+++ javascript/extraNetworks.js
@@ -74,14 +74,8 @@ function tryToRemoveExtraNetworkFromPrompt(textarea, text){
     return false
 }
 
-function cardClicked(tabname, textToAdd, allowNegativePrompt, descriptionText){
+function cardClicked(tabname, textToAdd, allowNegativePrompt){
     var textarea = allowNegativePrompt ? activePromptTextarea[tabname] : gradioApp().querySelector("#" + tabname + "_prompt > label > textarea")
-
-    var description_textarea = gradioApp().querySelector("#" + tabname+ '_description_input > label > textarea')
-
-    description_textarea.value = descriptionText
-
-    updateInput(description_textarea)
 
     if(! tryToRemoveExtraNetworkFromPrompt(textarea, textToAdd)){
         textarea.value = textarea.value + opts.extra_networks_add_text_separator + textToAdd
