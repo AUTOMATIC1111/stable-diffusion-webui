ec00dee2b2fda3df1f2b5008bcf7ba1f502ccc8c
Matěj Račinský <racinsky.matej@seznam.cz>
2023-04-08 17:26:32 +0200

    added my notes and lots of magnet links

--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,9 @@ set PYTHON=C:\ProgramData\Anaconda3\envs\image_generation\python.exe
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--theme dark
+::set COMMANDLINE_ARGS=--theme dark --medvram
+::set COMMANDLINE_ARGS=--theme dark --lowvram
+::set COMMANDLINE_ARGS=--theme dark --use-cpu all --no-half
+
 
 call webui.bat
