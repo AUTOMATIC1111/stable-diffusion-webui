f4b332f0419e09cec6983edcd07aae2ee0c14c24
missionfloyd <missionfloyd@users.noreply.github.com>
2023-04-18 17:01:46 -0600

    Add "None" option to extra networks dropdowns

--- extensions-builtin/Lora/scripts/lora_script.py
+++ extensions-builtin/Lora/scripts/lora_script.py
@@ -52,5 +52,5 @@ script_callbacks.on_before_ui(before_ui)
 
 
 shared.options_templates.update(shared.options_section(('extra_networks', "Extra Networks"), {
-    "sd_lora": shared.OptionInfo("None", "Add Lora to prompt", gr.Dropdown, lambda: {"choices": [""] + [x for x in lora.available_loras]}, refresh=lora.list_available_loras),
+    "sd_lora": shared.OptionInfo("None", "Add Lora to prompt", gr.Dropdown, lambda: {"choices": ["None"] + [x for x in lora.available_loras]}, refresh=lora.list_available_loras),
 }))
