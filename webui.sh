434c67d1a6592ba54d4156103b218548a37e7e7f
anapnoe <124302297+anapnoe@users.noreply.github.com>
2023-04-20 21:49:42 +0300

    fix Install in linux broken due to webui.sh #65

--- webui.sh
+++ webui.sh
@@ -29,7 +29,7 @@ fi
 # Name of the subdirectory (defaults to stable-diffusion-webui)
 if [[ -z "${clone_dir}" ]]
 then
-    clone_dir="stable-diffusion-webui"
+    clone_dir="stable-diffusion-webui-ux"
 fi
 
 # python3 executable
@@ -148,7 +148,7 @@ else
     printf "\n%s\n" "${delimiter}"
     printf "Clone stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
-    "${GIT}" clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
+    "${GIT}" clone https://github.com/anapnoe/stable-diffusion-webui-ux.git "${clone_dir}"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
