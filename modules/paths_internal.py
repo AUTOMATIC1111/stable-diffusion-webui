df424d6d5123628741f34599d58e95c7c83b0d23
Vladimir Mandic <mandic00@live.com>
2023-04-20 17:18:31 -0400

    add models path

--- modules/paths_internal.py
+++ modules/paths_internal.py
@@ -12,9 +12,10 @@ default_sd_model_file = sd_model_file
 # Parse the --data-dir flag first so we can use it as a base for our other argument default values
 parser_pre = argparse.ArgumentParser(add_help=False)
 parser_pre.add_argument("--data-dir", type=str, default=os.path.dirname(os.path.dirname(os.path.realpath(__file__))), help="base path where all user data is stored",)
+parser_pre.add_argument("--models-dir", type=str, default="models", help="base path where all models are stored",)
 cmd_opts_pre = parser_pre.parse_known_args()[0]
 data_path = cmd_opts_pre.data_dir
 
-models_path = os.path.join(data_path, "models")
+models_path = os.path.join(cmd_opts_pre.models_dir)
 extensions_dir = os.path.join(data_path, "extensions")
-extensions_builtin_dir = os.path.join(script_path, "extensions-builtin")
+extensions_builtin_dir = os.path.join(data_path, "extensions-builtin")
