f760ee62d98c8ad50c56477b15d10ad87392f725
liangz77 <liangz77@qq.com>
2023-04-23 11:16:17 +0800

    bugfix, in api mode, it failed to load lora

--- webui.py
+++ webui.py
@@ -222,6 +222,7 @@ def api_only():
     setup_middleware(app)
     api = create_api(app)
 
+    modules.script_callbacks.before_ui_callback()
     modules.script_callbacks.app_started_callback(None, app)
 
     print(f"Startup time: {startup_timer.summary()}.")
