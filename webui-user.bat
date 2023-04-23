f56b7cf5df8d9cb1fbad4250416a8f5063aceaeb
Matěj Račinský <racinsky.matej@seznam.cz>
2023-04-23 03:05:07 +0200

    removed debug print

--- webui-user.bat
+++ webui-user.bat
@@ -4,6 +4,7 @@ set PYTHON=C:\ProgramData\Anaconda3\envs\image_generation\python.exe
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config-af.json
+::set COMMANDLINE_ARGS=--theme dark --ui-settings-file=config.json --ui-config-file=ui-config-default.json
 ::set COMMANDLINE_ARGS=--theme dark --medvram
 ::set COMMANDLINE_ARGS=--theme dark --lowvram
 ::set COMMANDLINE_ARGS=--theme dark --use-cpu all --no-half
