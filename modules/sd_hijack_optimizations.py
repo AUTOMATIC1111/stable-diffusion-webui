8b75033a111e41d185cbef782b60520559a20e20
Seunghoon Lee <lshqqytiger@naver.com>
2023-04-26 12:34:27 +0900

    fix

--- modules/sd_hijack_optimizations.py
+++ modules/sd_hijack_optimizations.py
@@ -202,7 +202,7 @@ def einsum_op_cuda(q, k, v):
     return einsum_op_tensor_mem(q, k, v, mem_free_total / 3.3 / (1 << 20))
 
 def einsum_op_dml(q, k, v):
-    mem_total, mem_active = devices.adl.memory_stats()
+    mem_total, mem_active = torch.dml.memory_stats(q.device)
     mem_reserved = mem_total / (1 << 20) * 0.7
     return einsum_op_tensor_mem(q, k, v, (mem_reserved - mem_active) if mem_reserved > mem_active else 1)
 
