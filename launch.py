1bab1797c0334bdc829c3ee4481316385fd5dbe6
AUTOMATIC <16777216c@gmail.com>
2023-04-29 17:07:21 +0300

    use parsed commandline args for --skip-install

--- launch.py
+++ launch.py
@@ -120,12 +120,12 @@ def run_python(code, desc=None, errdesc=None):
     return run(f'"{python}" -c "{code}"', desc, errdesc)
 
 
-def run_pip(args, desc=None, live=False):
-    if "--skip-install" in sys.argv:
+def run_pip(command, desc=None, live=False):
+    if args.skip_install:
         return
 
     index_url_line = f' --index-url {index_url}' if index_url != '' else ''
-    return run(f'"{python}" -m pip {args} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
+    return run(f'"{python}" -m pip {command} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
 
 
 def check_run_python(code):
