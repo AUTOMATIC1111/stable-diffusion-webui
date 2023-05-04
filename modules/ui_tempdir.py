91a15dca80f44e32461f0ab7219de632781a2622
Sakura-Luna <53183413+Sakura-Luna@users.noreply.github.com>
2023-05-04 14:38:15 +0800

    Use a new way to solve webpage refresh

--- modules/ui_tempdir.py
+++ modules/ui_tempdir.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 from collections import namedtuple
 from pathlib import Path
+from time import time
 
 import gradio as gr
 
@@ -34,7 +35,7 @@ def check_tmp_file(gradio, filename):
 def save_pil_to_file(pil_image, dir=None):
     already_saved_as = getattr(pil_image, 'already_saved_as', None)
     if already_saved_as and os.path.isfile(already_saved_as):
-        register_tmp_file(shared.demo, already_saved_as)
+        register_tmp_file(shared.demo, f'{already_saved_as}?{int(time())}')
 
         file_obj = Savedfile(already_saved_as)
         return file_obj
