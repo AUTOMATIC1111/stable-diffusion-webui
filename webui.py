cf07983a6e5aa2cf131a75e5b974c25c171a7126
Sakura-Luna <53183413+Sakura-Luna@users.noreply.github.com>
2023-05-28 20:42:19 +0800

    Upgrade xformers

--- webui.py
+++ webui.py
@@ -135,7 +135,7 @@ there are reports of issues with training tab on the latest version.
 Use --skip-version-check commandline argument to disable this check.
         """.strip())
 
-    expected_xformers_version = "0.0.17"
+    expected_xformers_version = "0.0.20"
     if shared.xformers_available:
         import xformers
 
