123641e4ec93c0cd99f7ab03f4f39ca8ae4b4cef
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-05-30 01:06:23 +0900

    Revert "fix xyz clip"
    This reverts commit edd766e70ae3ea8039e7b9e24d55fbd26792ef10.

--- scripts/xyz_grid.py
+++ scripts/xyz_grid.py
@@ -94,10 +94,7 @@ def confirm_checkpoints(p, xs):
 
 
 def apply_clip_skip(p, x, xs):
-    if opts.data["CLIP_stop_at_last_layers"] != x:
-        opts.data["CLIP_stop_at_last_layers"] = x
-        p.cached_c = [None, None]
-        p.cached_uc = [None, None]
+    opts.data["CLIP_stop_at_last_layers"] = x
 
 
 def apply_upscale_latent_space(p, x, xs):
