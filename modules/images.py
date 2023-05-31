72f6367b9b34a1aa3f8743f4d9c2407bb9d2afeb
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-06-01 06:24:37 +0900

    fix 10896 pnginfo parameters

--- modules/images.py
+++ modules/images.py
@@ -504,8 +504,6 @@ def save_image_with_geninfo(image, geninfo, filename, extension=None, existing_p
     image_format = Image.registered_extensions()[extension]
 
     existing_pnginfo = existing_pnginfo or {}
-    if opts.enable_pnginfo:
-        existing_pnginfo['parameters'] = geninfo
 
     if extension.lower() == '.png':
         if opts.enable_pnginfo:
