31f57455dd606823b637042d701128ec4893c341
ramyma <ramy.mahmoudi@gmail.com>
2023-06-01 21:42:10 +0300

    Round upscaled dimensions only when not divisible by 8

--- modules/upscaler.py
+++ modules/upscaler.py
@@ -53,8 +53,8 @@ class Upscaler:
 
     def upscale(self, img: PIL.Image, scale, selected_model: str = None):
         self.scale = scale
-        dest_w = round((img.width * scale - 4) / 8) * 8
-        dest_h = round((img.height * scale - 4) / 8) * 8
+        dest_w = round((img.width * scale - 4) / 8) * 8 if img.width * scale % 8 != 0 else img.width * scale
+        dest_h = round((img.height * scale - 4) / 8) * 8 if img.height * scale % 8 != 0 else img.height * scale
 
         for _ in range(3):
             shape = (img.width, img.height)
