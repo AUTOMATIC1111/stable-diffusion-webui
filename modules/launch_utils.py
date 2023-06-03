df5a3cbefe46dbd6b5eb5b6a62e66d5b7378d4ee
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-06-03 13:33:23 +0900

    fallback version info form CHANGELOG.md

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -69,7 +69,8 @@ def git_tag():
         return subprocess.check_output([git, "describe", "--tags"], shell=False, encoding='utf8').strip()
     except Exception:
         try:
-            changelog_md = pathlib.Path(__file__).parent.parent / "CHANGELOG.md"
+            from pathlib import Path
+            changelog_md = Path(__file__).parent.parent / "CHANGELOG.md"
             with changelog_md.open(encoding="utf-8") as file:
                 return next((line.strip() for line in file if line.strip()), "<none>")
         except Exception:
