8e0fc7741b85d9904844ddd226d49bcb237b9c58
wangdongming <wangdongming@hifive.ai>
2023-06-06 16:25:28 +0800

    [feat](*): update

--- trainx/typex.py
+++ trainx/typex.py
@@ -231,6 +231,9 @@ class AdvancedConfig(SerializationObj):
                                        'ddim')  # ["ddim","pndm","lms","euler","euler_a","heun","dpm_2","dpm_2_a","dpmsolver","dpmsolver++","dpmsingle","k_lms","k_euler","k_euler_a","k_dpm_2","k_dpm_2_a",]
         self.set_property_value(task, 'sample_every_n_epochs', None)
         self.set_property_value(task, 'network_module', "networks.lora")
+        network_module = getattr(self, 'network_module')
+        if network_module and not str(network_module).startswith('networks'):
+            setattr(self, 'network_module', 'networks.' + network_module)
         self.set_property_value(task, 'conv_dim', None)
         self.set_property_value(task, 'conv_alpha')
         self.set_property_value(task, 'unit', 8)
@@ -256,6 +259,8 @@ class AdvancedConfig(SerializationObj):
         self.set_property_value(task, 'adaptive_noise_scale')
         self.set_property_value(task, 'multires_noise_iterations')
         self.set_property_value(task, 'multires_noise_discount', 0.3)
+
+
 
 
 class TrainLoraParams(SerializationObj):
@@ -388,6 +393,16 @@ class TrainLoraTask(UserDict):
             else:
                 num_repeats.append(params.train.num_repeats)
 
+        reg_tokens, list_reg_data_dir, list_reg_repeats = [], [], []
+        for i, item in enumerate(params.advanced.reg_tokens or []):
+            sub_folder = item['sub_folder'].replace(' ', "")
+            tokens = item['trigger']
+            list_reg_repeats.append(os.path.join(image_dir, sub_folder))
+            reg_tokens.append(tokens)
+            if isinstance(params.advanced.list_reg_repeats, dict):
+                num_repeats.append(params.advanced.list_reg_repeats[sub_folder])
+            else:
+                num_repeats.append(params.advanced.list_reg_repeats)
         key = self.hash_id[:32]
 
         kwargs = {
@@ -422,6 +437,12 @@ class TrainLoraTask(UserDict):
 
         }
         kwargs.update(params.advanced.to_dict())
+        kwargs.update({
+            # reg_tokens, list_reg_data_dir, list_reg_repeats
+            'list_reg_repeats': list_reg_repeats,
+            'list_reg_data_dir': list_reg_data_dir,
+            'reg_tokens': reg_tokens
+        })
 
         return kwargs
 
