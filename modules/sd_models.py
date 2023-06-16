41363e0d27bbaa0c84eebe3c7c8451075390ec4e
dhwz <dhwz@gmx.net>
2023-06-16 18:10:15 +0200

    fix very slow loading speed of .safetensors files

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -247,8 +247,11 @@ def read_metadata_from_safetensors(filename):
 def read_state_dict(checkpoint_file, print_global_state=False, map_location=None):
     _, extension = os.path.splitext(checkpoint_file)
     if extension.lower() == ".safetensors":
-        device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
-        pl_sd = safetensors.torch.load_file(checkpoint_file, device=device)
+        if not shared.opts.disable_mmap_load_safetensors:
+            device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
+            pl_sd = safetensors.torch.load_file(checkpoint_file, device=device)
+        else:
+            pl_sd = safetensors.torch.load(open(checkpoint_file, 'rb').read())
     else:
         pl_sd = torch.load(checkpoint_file, map_location=map_location or shared.weight_load_location)
 
