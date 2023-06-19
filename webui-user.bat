da4cee065e70afb32b6b16340e3e1c04b4b8e035
boombbo <boombbo@qq.com>
2023-06-20 03:09:37 +0800

    master

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--autolaunch  --no-gradio-queue  --allow-code --no-half-vae --no-half --api  --xformers  --skip-version-check --skip-torch-cuda-test 
+git pull
+
 
 call webui.bat
