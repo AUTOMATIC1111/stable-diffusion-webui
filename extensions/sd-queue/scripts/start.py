c410a9082069637ee5810beca4730720468ef1de
liuzh <liuzh@liuzh.com>
2023-06-28 22:00:25 +0800

    optimization

--- extensions/sd-queue/scripts/start.py
+++ extensions/sd-queue/scripts/start.py
@@ -2,11 +2,11 @@ import sys
 from fastapi import FastAPI
 
 import gradio as gr
+from scripts.taskListener import TaskListener
 
 
 def start_queue(_: gr.Blocks, app: FastAPI):
     if '--start-task-listener' in sys.argv:
-        from scripts.taskListener import TaskListener
         print(f"Launching API server with task listener")
         task = TaskListener(app)
         task.start()
