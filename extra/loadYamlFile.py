affea5ad8c87e2880762a6493de2cf637fc160fd
liuzh <liuzh@liuzh.com>
2023-07-01 09:15:38 +0800

    optimization

--- extra/loadYamlFile.py
+++ extra/loadYamlFile.py
@@ -8,6 +8,8 @@ class ExtraConfig:
     def __init__(self, environment=None):
         if environment is not None:
             environment = f"-{environment}"
+        else:
+            environment = ""
         self.file_path = os.path.join(sd_configs_path, f"sd-extra{environment}.yaml")
         self.config = None
 
