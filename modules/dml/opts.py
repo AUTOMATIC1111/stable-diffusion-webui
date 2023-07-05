a076ff1b4332e136b6e2be967b33d6915e75167b
Seunghoon Lee <lshqqytiger@naver.com>
2023-07-06 01:45:12 +0900

    Use backend enum on diffusers check.

--- modules/dml/opts.py
+++ modules/dml/opts.py
@@ -1,5 +1,5 @@
 from modules import shared
 
 def override_opts():
-    if shared.cmd_opts.backend.lower() == "diffusers":
+    if shared.backend == shared.Backend.DIFFUSERS:
         shared.opts.diffusers_generator_device = "cpu" # DirectML does not support torch.Generator API.
