1d36e19996d7ceafc5283e54eda02c5ae9dd403b
Seunghoon Lee <lshqqytiger@naver.com>
2023-07-07 18:52:04 +0900

    Fix PNDMScheduler for DirectML.

--- modules/dml/hijack/__init__.py
+++ modules/dml/hijack/__init__.py
@@ -3,3 +3,4 @@ import modules.dml.hijack.stablediffusion
 import modules.dml.hijack.torch
 import modules.dml.hijack.realesrgan_model
 import modules.dml.hijack.plms
+import modules.dml.hijack.diffusers
