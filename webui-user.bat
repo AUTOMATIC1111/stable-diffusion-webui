85c0dd6d24d4b3a97e64465c9167bc5938b86af5
Charlie Joynt <cjj1977@users.noreply.github.com>
2023-07-14 22:19:50 +0100

    change default data locations

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,29 @@
 @echo off
 
-set PYTHON=
+git pull
+
+set SD_ROOT=S:/StableDiffusion/data
+
+set PYTHON=python3.10.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=^
+ --listen^
+ --xformers^
+ --theme=dark^
+ --bsrgan-models-path=%SD_ROOT%/bsrgan^
+ --ckpt-dir=%SD_ROOT%/checkpoints^
+ --clip-models-path=%SD_ROOT%/clip^
+ --codeformer-models-path=%SD_ROOT%/codeformer^
+ --embeddings-dir=%SD_ROOT%/embeddings^
+ --esrgan-models-path=%SD_ROOT%/esrgan^
+ --gfpgan-dir=%SD_ROOT%/gfpgan^
+ --gfpgan-models-path=%SD_ROOT%/gfpgan^
+ --ldsr-models-path=%SD_ROOT%/ldsr^
+ --lora-dir=%SD_ROOT%/lora^
+ --realesrgan-models-path=%SD_ROOT%/realesrgan^
+ --scunet-models-path=%SD_ROOT%/scunet^
+ --swinir-models-path=%SD_ROOT%/swinir^
+ --vae-dir=%SD_ROOT%/vae
 
 call webui.bat
