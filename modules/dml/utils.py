6b4b863fd0ac645f407dcaaefd61a73c2da99b29
Seunghoon Lee <lshqqytiger@naver.com>
2023-07-15 19:03:10 +0900

    simplify get_device.

--- modules/dml/utils.py
+++ modules/dml/utils.py
@@ -4,5 +4,5 @@ import torch
 rDevice = torch.device | int
 def get_device(device: Optional[rDevice]=None) -> torch.device:
     if device is None:
-        device = torch.dml.context_device or torch.dml.current_device()
+        device = torch.dml.current_device()
     return torch.device(device)
