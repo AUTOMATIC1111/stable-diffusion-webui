436e46124d03eba8614b2fe8af174fe136c2ec81
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-07-17 12:42:38 +0200

    typo

--- docker/run.sh
+++ docker/run.sh
@@ -10,5 +10,5 @@ fi
 $compose up -d
 
 echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker compose ps --status running --format json | sed -e 's/.*"PublishedPort":\([0-9]\+\),.*/\1/g') \n\n"
-echo "To stop showing the logs precc CTRL-C"
+echo "To stop showing the logs press CTRL-C"
 $compose logs -f
