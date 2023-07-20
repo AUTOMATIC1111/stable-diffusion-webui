7578f3ac326e604be27011ccec2c8328db5886e7
wangdongming <wangdongming@hifive.ai>
2023-07-20 21:43:04 +0800

    [feat](*): add adetailer and ui

---
+++ worker/k8s_health.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/7/20 9:38 PM
+# @Author  : wangdongming
+# @Site    : 
+# @File    : k8s_health.py
+# @Software: Hifive
+
+def write_healthy(status: bool):
+    with open("/var/healthy.txt") as f:
+        v = "0" if status else '1'
+        f.write(v)
+
+
