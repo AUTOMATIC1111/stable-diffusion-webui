511a8cbb61f2c9c09e64a96df0b801e7e73a4236
Vladimir Mandic <mandic00@live.com>
2023-07-21 15:04:27 -0400

    fix en hypernetwork

--- modules/ui_extra_networks_hypernets.py
+++ modules/ui_extra_networks_hypernets.py
@@ -13,13 +13,22 @@ class ExtraNetworksPageHypernetworks(ui_extra_networks.ExtraNetworksPage):
     def list_items(self):
         for name, path in shared.hypernetworks.items():
             path, _ext = os.path.splitext(path)
+            print('HERE', {
+                "name": name,
+                "filename": path,
+                "preview": self.find_preview(path),
+                "description": self.find_description(path),
+                "search_term": self.search_terms_from_path(path),
+                "prompt": json.dumps(f"<hypernet:{name}:{shared.opts.extra_networks_default_multiplier}>"),
+                "local_preview": f"{path}.preview.{shared.opts.samples_format}",
+            })
             yield {
                 "name": name,
                 "filename": path,
                 "preview": self.find_preview(path),
                 "description": self.find_description(path),
                 "search_term": self.search_terms_from_path(path),
-                "prompt": json.dumps(f"<hypernet:{name}:") + " + shared.opts.extra_networks_default_multiplier + " + json.dumps(">"),
+                "prompt": json.dumps(f"<hypernet:{name}:{shared.opts.extra_networks_default_multiplier}>"),
                 "local_preview": f"{path}.preview.{shared.opts.samples_format}",
             }
 
