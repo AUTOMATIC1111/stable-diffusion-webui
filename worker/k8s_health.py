5db0582f311bf268313be1f6ae3dc2f44efee4eb
wangdongming <wangdongming@hifive.ai>
2023-07-21 19:33:36 +0800

    [feat](*):adetailer default enable

--- worker/k8s_health.py
+++ worker/k8s_health.py
@@ -5,10 +5,15 @@
 # @Site    : 
 # @File    : k8s_health.py
 # @Software: Hifive
+import os
+
 
 def write_healthy(status: bool):
-    with open("/var/healthy.txt", "w+") as f:
-        v = "0" if status else '1'
-        f.write(v)
+    if status:
+        with open("/var/healthy.txt", "w+") as f:
+            v = "0" if status else '1'
+            f.write(v)
+    elif os.path.isfile("/var/healthy.txt"):
+        os.remove("/var/healthy.txt")
 
 
