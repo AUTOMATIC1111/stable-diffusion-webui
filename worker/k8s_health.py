3ac7f7aa2026e05c324683a3631eb782c4f6ffa9
wangdongming <wangdongming@hifive.ai>
2023-07-24 14:28:35 +0800

    [feat](*):update webui startup

--- worker/k8s_health.py
+++ worker/k8s_health.py
@@ -6,6 +6,7 @@
 # @File    : k8s_health.py
 # @Software: Hifive
 import os
+from loguru import logger
 
 
 def write_healthy(status: bool):
@@ -16,4 +17,16 @@ def write_healthy(status: bool):
     elif os.path.isfile("/var/healthy.txt"):
         os.remove("/var/healthy.txt")
 
+
+def system_exit(free, threshold=8):
+    if free / 2 ** 30 < threshold:
+        logger.info("CUDA out of memory, quit...")
+        # kill process
+        from ctypes import CDLL
+        from ctypes.util import find_library
+
+        write_healthy(False)
+
+        libc = CDLL(find_library("libc"))
+        libc.exit(1)
 
