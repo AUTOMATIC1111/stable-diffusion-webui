df97caaef9acb413b74e9700a2c58e1ba212c8e3
Charlie Joynt <cjj1977@users.noreply.github.com>
2023-07-27 01:36:16 +0100

    HF_DATASETS_CACHE

--- webui-user.bat
+++ webui-user.bat
@@ -2,7 +2,8 @@
 
 git pull upstream master
 
-set SD_ROOT=S:/StableDiffusion/data
+set HF_DATASETS_CACHE=S:/HuggingFace/.cache/datasets
+set SD_DATA=S:/StableDiffusion/data
 
 set PYTHON=python3.10.exe
 set GIT=
@@ -12,19 +13,19 @@ set COMMANDLINE_ARGS=^
  --listen^
  --xformers^
  --theme=dark^
- --bsrgan-models-path=%SD_ROOT%/bsrgan^
- --ckpt-dir=%SD_ROOT%/checkpoints^
- --clip-models-path=%SD_ROOT%/clip^
- --codeformer-models-path=%SD_ROOT%/codeformer^
- --embeddings-dir=%SD_ROOT%/embeddings^
- --esrgan-models-path=%SD_ROOT%/esrgan^
- --gfpgan-dir=%SD_ROOT%/gfpgan^
- --gfpgan-models-path=%SD_ROOT%/gfpgan^
- --ldsr-models-path=%SD_ROOT%/ldsr^
- --lora-dir=%SD_ROOT%/lora^
- --realesrgan-models-path=%SD_ROOT%/realesrgan^
- --scunet-models-path=%SD_ROOT%/scunet^
- --swinir-models-path=%SD_ROOT%/swinir^
- --vae-dir=%SD_ROOT%/vae
+ --bsrgan-models-path=%SD_DATA%/bsrgan^
+ --ckpt-dir=%SD_DATA%/checkpoints^
+ --clip-models-path=%SD_DATA%/clip^
+ --codeformer-models-path=%SD_DATA%/codeformer^
+ --embeddings-dir=%SD_DATA%/embeddings^
+ --esrgan-models-path=%SD_DATA%/esrgan^
+ --gfpgan-dir=%SD_DATA%/gfpgan^
+ --gfpgan-models-path=%SD_DATA%/gfpgan^
+ --ldsr-models-path=%SD_DATA%/ldsr^
+ --lora-dir=%SD_DATA%/lora^
+ --realesrgan-models-path=%SD_DATA%/realesrgan^
+ --scunet-models-path=%SD_DATA%/scunet^
+ --swinir-models-path=%SD_DATA%/swinir^
+ --vae-dir=%SD_DATA%/vae
 
 call webui.bat
