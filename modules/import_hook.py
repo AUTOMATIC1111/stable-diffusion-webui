f67f856fda1eaffbd4e740f9cbc7a9f1304c89e0
diffusion <diffusion@pop-os.localdomain>
2023-07-28 21:19:19 +0300

    updated import_hook to imprto xformers. this change makes the module available to sd_hijacking with xformers enable

--- modules/import_hook.py
+++ modules/import_hook.py
@@ -1,5 +1,7 @@
 import sys
 
-# this will break any attempt to import xformers which will prevent stability diffusion repo from trying to use it
+# solution to load xformers by @liuzimo https://github.com/AUTOMATIC1111/stable-diffusion-webui/discussions/6594#discussioncomment-5216054
+# sd_hijack will fail to load xformers because sys.modules cannot find i. This solution solves the problem. 
 if "--xformers" not in "".join(sys.argv):
-    sys.modules["xformers"] = None
+    import xformers
+sys.modules["xformers"] = xformers
