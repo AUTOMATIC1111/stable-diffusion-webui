24bc2a4a035651b0e3a22a7b7f76bfe8d804b707
Charlie Joynt <cjj1977@users.noreply.github.com>
2023-07-31 10:27:15 +0100

    updated webui launch parameters

--- webui-user.bat
+++ webui-user.bat
@@ -1,31 +1,44 @@
 @echo off
 
-git pull upstream master
+:: The local stable-diffusion-webui folder is a fork of the AUTOMATIC1111 repo.
+:: A "remote" has been added called "A1111" that points at the original repo so
+:: we pull the latest changes into the forked one when starting the webui:
+git pull A1111 master
 
-set HF_DATASETS_CACHE=S:/HuggingFace/.cache/datasets
-set SD_DATA=S:/StableDiffusion/data
+:: A (futile?) attempt to move massive files off the C drive:
+set HF_DATASETS_CACHE=S:\\HuggingFace\\.cache\\datasets
+set TRANSFORMERS_CACHE=S:\\HuggingFace\\.cache\\transformers
 
 set PYTHON=python3.10.exe
 set GIT=
-set VENV_DIR=
+set VENV_DIR=S:\\StableDiffusion\\venv
+
+set SD_DATA=S:\\StableDiffusion\\data
 set COMMANDLINE_ARGS=^
+ --data-dir=%SD_DATA%^
+ --ckpt-dir=%SD_DATA%\\models\\Checkpoints^
+ --vae-dir=%SD_DATA%\\models\\VAE^
  --enable-insecure-extension-access^
  --listen^
- --xformers^
+ --no-half-vae^
  --theme=dark^
- --bsrgan-models-path=%SD_DATA%/bsrgan^
- --ckpt-dir=%SD_DATA%/checkpoints^
- --clip-models-path=%SD_DATA%/clip^
- --codeformer-models-path=%SD_DATA%/codeformer^
- --embeddings-dir=%SD_DATA%/embeddings^
- --esrgan-models-path=%SD_DATA%/esrgan^
- --gfpgan-dir=%SD_DATA%/gfpgan^
- --gfpgan-models-path=%SD_DATA%/gfpgan^
- --ldsr-models-path=%SD_DATA%/ldsr^
- --lora-dir=%SD_DATA%/lora^
- --realesrgan-models-path=%SD_DATA%/realesrgan^
- --scunet-models-path=%SD_DATA%/scunet^
- --swinir-models-path=%SD_DATA%/swinir^
- --vae-dir=%SD_DATA%/vae
+ --xformers
+
+:: data-dir argument
+::
+:: This sets the root path for the following folders. Useful to avoid losing
+:: large downloads if you delete the main stable-diffusion-webui folder.
+::
+:: + extensions
+:: + models
+::   + Codeformer
+::   + ControlNet
+::   + ESRGAN
+::   + GFPGAN
+::   + hypernetworks
+::   + LDSR
+::   + Lora
+::   + Stable-diffusion
+::   + SwinIR
 
 call webui.bat
