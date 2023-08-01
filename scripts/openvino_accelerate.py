c111b00ae864972032bd96a06fb8249f5a51da62
ynimmaga <yamini.nimmagadda@intel.com>
2023-08-01 14:39:35 -0700

    Added changes to diffusers from_single_file

--- scripts/openvino_accelerate.py
+++ scripts/openvino_accelerate.py
@@ -24,6 +24,7 @@ from modules.sd_models import CheckpointInfo
 from modules.shared import Shared, opts, state
 
 from PIL import Image, ImageOps
+from pathlib import Path
 
 import openvino.frontend.pytorch.torchdynamo.backend
 from openvino.frontend.pytorch.torchdynamo.execute import partitioned_modules, compiled_cache
@@ -42,6 +43,14 @@ from diffusers import (
     PNDMScheduler,
 )
 
+from diffusers.pipelines.stable_diffusion.convert_from_ckpt import download_from_original_stable_diffusion_ckpt
+
+from diffusers.utils import (
+    DIFFUSERS_CACHE,
+    HF_HUB_OFFLINE,
+    is_safetensors_available,
+)
+
 class ModelState:
     def __init__(self):
         self.recompile = 1
@@ -52,6 +61,116 @@ class ModelState:
         self.mode = 0
 
 model_state = ModelState()
+
+def from_single_file(self, pretrained_model_link_or_path, **kwargs):
+
+    cache_dir = kwargs.pop("cache_dir", DIFFUSERS_CACHE)
+    resume_download = kwargs.pop("resume_download", False)
+    force_download = kwargs.pop("force_download", False)
+    proxies = kwargs.pop("proxies", None)
+    local_files_only = kwargs.pop("local_files_only", HF_HUB_OFFLINE)
+    use_auth_token = kwargs.pop("use_auth_token", None)
+    revision = kwargs.pop("revision", None)
+    extract_ema = kwargs.pop("extract_ema", False)
+    image_size = kwargs.pop("image_size", None)
+    scheduler_type = kwargs.pop("scheduler_type", "pndm")
+    num_in_channels = kwargs.pop("num_in_channels", None)
+    upcast_attention = kwargs.pop("upcast_attention", None)
+    load_safety_checker = kwargs.pop("load_safety_checker", True)
+    prediction_type = kwargs.pop("prediction_type", None)
+    text_encoder = kwargs.pop("text_encoder", None)
+    tokenizer = kwargs.pop("tokenizer", None)
+    local_config_file = kwargs.pop("local_config_file", None)
+  
+    torch_dtype = kwargs.pop("torch_dtype", None)
+
+    use_safetensors = kwargs.pop("use_safetensors", None if is_safetensors_available() else False)
+
+    pipeline_name = self.__name__
+    file_extension = pretrained_model_link_or_path.rsplit(".", 1)[-1]
+    from_safetensors = file_extension == "safetensors"
+
+    if from_safetensors and use_safetensors is False:
+        raise ValueError("Make sure to install `safetensors` with `pip install safetensors`.")
+
+    # TODO: For now we only support stable diffusion
+    stable_unclip = None
+    model_type = None
+    controlnet = False
+
+    if pipeline_name == "StableDiffusionControlNetPipeline":
+        # Model type will be inferred from the checkpoint.
+        controlnet = True
+    elif "StableDiffusion" in pipeline_name:
+        # Model type will be inferred from the checkpoint.
+        pass
+    elif pipeline_name == "StableUnCLIPPipeline":
+        model_type = "FrozenOpenCLIPEmbedder"
+        stable_unclip = "txt2img"
+    elif pipeline_name == "StableUnCLIPImg2ImgPipeline":
+        model_type = "FrozenOpenCLIPEmbedder"
+        stable_unclip = "img2img"
+    elif pipeline_name == "PaintByExamplePipeline":
+        model_type = "PaintByExample"
+    elif pipeline_name == "LDMTextToImagePipeline":
+        model_type = "LDMTextToImage"
+    else:
+        raise ValueError(f"Unhandled pipeline class: {pipeline_name}")
+
+    # remove huggingface url
+    for prefix in ["https://huggingface.co/", "huggingface.co/", "hf.co/", "https://hf.co/"]:
+        if pretrained_model_link_or_path.startswith(prefix):
+            pretrained_model_link_or_path = pretrained_model_link_or_path[len(prefix) :]
+    # Code based on diffusers.pipelines.pipeline_utils.DiffusionPipeline.from_pretrained
+    ckpt_path = Path(pretrained_model_link_or_path)
+    if not ckpt_path.is_file():
+        # get repo_id and (potentially nested) file path of ckpt in repo
+        repo_id = "/".join(ckpt_path.parts[:2])
+        file_path = "/".join(ckpt_path.parts[2:])
+
+        if file_path.startswith("blob/"):
+            file_path = file_path[len("blob/") :]
+
+        if file_path.startswith("main/"):
+            file_path = file_path[len("main/") :]
+
+        pretrained_model_link_or_path = hf_hub_download(
+            repo_id,
+            filename=file_path,
+            cache_dir=cache_dir,
+            resume_download=resume_download,
+            proxies=proxies,
+            local_files_only=local_files_only,
+            use_auth_token=use_auth_token,
+            revision=revision,
+            force_download=force_download,
+        )
+
+    pipe = download_from_original_stable_diffusion_ckpt(
+        pretrained_model_link_or_path,
+        original_config_file=local_config_file,
+        pipeline_class=self,
+        model_type=model_type,
+        stable_unclip=stable_unclip,
+        controlnet=controlnet,
+        from_safetensors=from_safetensors,
+        extract_ema=extract_ema,
+        image_size=image_size,
+        scheduler_type=scheduler_type,
+        num_in_channels=num_in_channels,
+        upcast_attention=upcast_attention,
+        load_safety_checker=load_safety_checker,
+        prediction_type=prediction_type,
+        text_encoder=text_encoder,
+        tokenizer=tokenizer,
+    )
+
+    if torch_dtype is not None:
+        pipe.to(torch_dtype=torch_dtype)
+
+    return pipe
+
+StableDiffusionPipeline.from_single_file = functools.partial(from_single_file, StableDiffusionPipeline)
 
 def openvino_clear_caches():
     global partitioned_modules
@@ -98,10 +217,12 @@ def get_diffusers_sd_model(sampler_name, enable_caching, openvino_device, mode):
         torch._dynamo.reset()
         openvino_clear_caches()
         curr_dir_path = os.getcwd()
-        model_path = "/models/Stable-diffusion/"
+        #model_path = "/models/Stable-diffusion/"
         checkpoint_name = shared.opts.sd_model_checkpoint.split(" ")[0]
-        checkpoint_path = curr_dir_path + model_path + checkpoint_name
-        sd_model = StableDiffusionPipeline.from_single_file(checkpoint_path, load_safety_checker=False)
+        checkpoint_path = os.path.join(curr_dir_path, 'models', 'Stable-diffusion', checkpoint_name)
+        config_name = checkpoint_name.split(".")[0] + ".yaml"
+        local_config_file = os.path.join(curr_dir_path, 'configs',config_name)
+        sd_model = StableDiffusionPipeline.from_single_file(checkpoint_path, local_config_file=local_config_file, load_safety_checker=False)
         if (mode == 1):
             sd_model = StableDiffusionImg2ImgPipeline(**sd_model.components)
         elif (mode == 2):
