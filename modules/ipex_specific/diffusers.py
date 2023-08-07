dd4412734e32895879f3f3cbfb5a42715d43c7be
Disty0 <semihgulec2005@gmail.com>
2023-08-07 11:32:00 +0300

    IPEX add torchvision on Windows

--- modules/ipex_specific/diffusers.py
+++ modules/ipex_specific/diffusers.py
@@ -58,8 +58,12 @@ class SlicedAttnProcessor:
         if block_size >= 4000:
             do_split_2 = True
             #Find something divisible with the query_tokens
+            sanity_check = 0
             while ((self.slice_size * split_2_slice_size * shape_three) / 1024 * block_multiply) > 4000:
                 split_2_slice_size = split_2_slice_size // 2
+                sanity_check = sanity_check + 1
+                if sanity_check >= 128:
+                    break
         else:
             do_split_2 = False
 
