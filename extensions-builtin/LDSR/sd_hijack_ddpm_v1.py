dad5fcff0c6396e6fa4cedabb9ccc66e8bd61b66
Olivier Lacan <hi@olivierlacan.com>
2023-08-07 12:10:58 -0700

    Update outdated pytorch_lightning namespace
    The existing imports results in this error:
    ModuleNotFoundError: No module named 'pytorch_lightning.utilities.distributed'
    
    This is documented in #11458 but no fix was
    made to the repo itself.
    
    I'm not sure it's a safe change but it never hurts to ask.

--- extensions-builtin/LDSR/sd_hijack_ddpm_v1.py
+++ extensions-builtin/LDSR/sd_hijack_ddpm_v1.py
@@ -14,7 +14,7 @@ from contextlib import contextmanager
 from functools import partial
 from tqdm import tqdm
 from torchvision.utils import make_grid
-from pytorch_lightning.utilities.distributed import rank_zero_only
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
 from ldm.util import log_txt_as_img, exists, default, ismap, isimage, mean_flat, count_params, instantiate_from_config
 from ldm.modules.ema import LitEma
