8b7b99f8d582edf6422e52f41b82f08cdb80568d
catboxanon <122327233+catboxanon@users.noreply.github.com>
2023-08-09 12:18:03 -0400

    fix: Only import tqdm when needed

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -7,7 +7,6 @@ import importlib.util
 import platform
 import json
 from functools import lru_cache
-import tqdm
 
 from modules import cmd_args, errors
 from modules.paths_internal import script_path, extensions_dir
@@ -249,6 +248,7 @@ def run_extensions_installers(settings_file):
         return
 
     with startup_timer.subcategory("run extensions installers"):
+        import tqdm
         progress_bar = tqdm.tqdm(list_extensions(settings_file))
         for dirname_extension in progress_bar:
             progress_bar.set_description(f"Installing {dirname_extension}")
