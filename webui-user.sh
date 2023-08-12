e7f85384ac337bd03b7af47f12844cc8da4f071f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-12 23:25:46 +0200

    Update webui-user.sh to call bash through /usr/bin/env
    /bin/bash does not exist on NixOS

--- webui-user.sh
+++ webui-user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #########################################################
 # Uncomment and change the variables below to your need:#
 #########################################################
