400fc3b109d59be40d40450ad9fd759b367c6b32
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-08-15 10:05:24 -0600

    Update outpainting_mk_2.py
    Based on my understanding, here I made a few optimizations:
    Removed redundant loop handling for different channels using NumPy broadcasting.
    Simplified the calculation of the masked window for RGB.
    Removed unused variables and optimized calculations for windowed_image and src_fft.
    Combined the calculation of the noise window and noise_rgb.
    Utilized array broadcasting to perform operations across multiple channels.
    Replaced explicit loops with array operations for noise shaping.
    Improved readability by removing redundant slicing operations.

--- scripts/outpainting_mk_2.py
+++ scripts/outpainting_mk_2.py
@@ -13,108 +13,76 @@ from modules.shared import opts, state
 
 
 # this function is taken from https://github.com/parlance-zz/g-diffuser-bot
-def get_matched_noise(_np_src_image, np_mask_rgb, noise_q=1, color_variation=0.05):
-    # helper fft routines that keep ortho normalization and auto-shift before and after fft
-    def _fft2(data):
-        if data.ndim > 2:  # has channels
-            out_fft = np.zeros((data.shape[0], data.shape[1], data.shape[2]), dtype=np.complex128)
-            for c in range(data.shape[2]):
-                c_data = data[:, :, c]
-                out_fft[:, :, c] = np.fft.fft2(np.fft.fftshift(c_data), norm="ortho")
-                out_fft[:, :, c] = np.fft.ifftshift(out_fft[:, :, c])
-        else:  # one channel
-            out_fft = np.zeros((data.shape[0], data.shape[1]), dtype=np.complex128)
-            out_fft[:, :] = np.fft.fft2(np.fft.fftshift(data), norm="ortho")
-            out_fft[:, :] = np.fft.ifftshift(out_fft[:, :])
+import numpy as np
+import skimage.exposure
 
-        return out_fft
+def get_matched_noise(_np_src_image, np_mask_rgb, noise_q=1, color_variation=0.05):
+    def _fft2(data):
+        out_fft = np.fft.fft2(np.fft.fftshift(data), norm="ortho")
+        return np.fft.ifftshift(out_fft)
 
     def _ifft2(data):
-        if data.ndim > 2:  # has channels
-            out_ifft = np.zeros((data.shape[0], data.shape[1], data.shape[2]), dtype=np.complex128)
-            for c in range(data.shape[2]):
-                c_data = data[:, :, c]
-                out_ifft[:, :, c] = np.fft.ifft2(np.fft.fftshift(c_data), norm="ortho")
-                out_ifft[:, :, c] = np.fft.ifftshift(out_ifft[:, :, c])
-        else:  # one channel
-            out_ifft = np.zeros((data.shape[0], data.shape[1]), dtype=np.complex128)
-            out_ifft[:, :] = np.fft.ifft2(np.fft.fftshift(data), norm="ortho")
-            out_ifft[:, :] = np.fft.ifftshift(out_ifft[:, :])
-
-        return out_ifft
+        out_ifft = np.fft.ifft2(np.fft.fftshift(data), norm="ortho")
+        return np.fft.ifftshift(out_ifft)
 
     def _get_gaussian_window(width, height, std=3.14, mode=0):
-        window_scale_x = float(width / min(width, height))
-        window_scale_y = float(height / min(width, height))
-
-        window = np.zeros((width, height))
+        window_scale_x = width / min(width, height)
+        window_scale_y = height / min(width, height)
         x = (np.arange(width) / width * 2. - 1.) * window_scale_x
-        for y in range(height):
-            fy = (y / height * 2. - 1.) * window_scale_y
-            if mode == 0:
-                window[:, y] = np.exp(-(x ** 2 + fy ** 2) * std)
-            else:
-                window[:, y] = (1 / ((x ** 2 + 1.) * (fy ** 2 + 1.))) ** (std / 3.14)  # hey wait a minute that's not gaussian
-
+        fy = np.outer(np.arange(height) / height * 2. - 1., np.ones(width))
+        if mode == 0:
+            window = np.exp(-(x ** 2 + fy ** 2) * std)
+        else:
+            window = (1 / ((x ** 2 + 1.) * (fy ** 2 + 1.))) ** (std / 3.14)
         return window
 
     def _get_masked_window_rgb(np_mask_grey, hardness=1.):
-        np_mask_rgb = np.zeros((np_mask_grey.shape[0], np_mask_grey.shape[1], 3))
-        if hardness != 1.:
-            hardened = np_mask_grey[:] ** hardness
-        else:
-            hardened = np_mask_grey[:]
-        for c in range(3):
-            np_mask_rgb[:, :, c] = hardened[:]
+        hardened = np_mask_grey ** hardness
+        np_mask_rgb = np.tile(hardened[:, :, np.newaxis], (1, 1, 3))
         return np_mask_rgb
 
-    width = _np_src_image.shape[0]
-    height = _np_src_image.shape[1]
-    num_channels = _np_src_image.shape[2]
+    width, height, num_channels = _np_src_image.shape
 
-    _np_src_image[:] * (1. - np_mask_rgb)
-    np_mask_grey = (np.sum(np_mask_rgb, axis=2) / 3.)
+    _np_src_image *= (1. - np_mask_rgb)
+    np_mask_grey = np.sum(np_mask_rgb, axis=2) / 3.
     img_mask = np_mask_grey > 1e-6
     ref_mask = np_mask_grey < 1e-3
 
     windowed_image = _np_src_image * (1. - _get_masked_window_rgb(np_mask_grey))
     windowed_image /= np.max(windowed_image)
-    windowed_image += np.average(_np_src_image) * np_mask_rgb  # / (1.-np.average(np_mask_rgb))  # rather than leave the masked area black, we get better results from fft by filling the average unmasked color
+    windowed_image += np.average(_np_src_image) * np_mask_rgb
 
-    src_fft = _fft2(windowed_image)  # get feature statistics from masked src img
-    src_dist = np.absolute(src_fft)
+    src_fft = _fft2(windowed_image)
+    src_dist = np.abs(src_fft)
     src_phase = src_fft / src_dist
 
-    # create a generator with a static seed to make outpainting deterministic / only follow global seed
     rng = np.random.default_rng(0)
 
-    noise_window = _get_gaussian_window(width, height, mode=1)  # start with simple gaussian noise
+    noise_window = _get_gaussian_window(width, height, mode=1)
     noise_rgb = rng.random((width, height, num_channels))
-    noise_grey = (np.sum(noise_rgb, axis=2) / 3.)
-    noise_rgb *= color_variation  # the colorfulness of the starting noise is blended to greyscale with a parameter
-    for c in range(num_channels):
-        noise_rgb[:, :, c] += (1. - color_variation) * noise_grey
+    noise_grey = np.sum(noise_rgb, axis=2) / 3.
+    noise_rgb *= color_variation
+    noise_rgb += (1. - color_variation) * noise_grey[:, :, np.newaxis]
 
     noise_fft = _fft2(noise_rgb)
-    for c in range(num_channels):
-        noise_fft[:, :, c] *= noise_window
+    noise_fft *= noise_window[:, :, np.newaxis]
     noise_rgb = np.real(_ifft2(noise_fft))
     shaped_noise_fft = _fft2(noise_rgb)
-    shaped_noise_fft[:, :, :] = np.absolute(shaped_noise_fft[:, :, :]) ** 2 * (src_dist ** noise_q) * src_phase  # perform the actual shaping
-
-    brightness_variation = 0.  # color_variation # todo: temporarily tieing brightness variation to color variation for now
-    contrast_adjusted_np_src = _np_src_image[:] * (brightness_variation + 1.) - brightness_variation * 2.
-
-    # scikit-image is used for histogram matching, very convenient!
+    shaped_noise_fft *= (src_dist ** noise_q) * src_phase
     shaped_noise = np.real(_ifft2(shaped_noise_fft))
     shaped_noise -= np.min(shaped_noise)
     shaped_noise /= np.max(shaped_noise)
-    shaped_noise[img_mask, :] = skimage.exposure.match_histograms(shaped_noise[img_mask, :] ** 1., contrast_adjusted_np_src[ref_mask, :], channel_axis=1)
-    shaped_noise = _np_src_image[:] * (1. - np_mask_rgb) + shaped_noise * np_mask_rgb
 
-    matched_noise = shaped_noise[:]
+    brightness_variation = 0.  # color_variation
+    contrast_adjusted_np_src = _np_src_image * (brightness_variation + 1.) - brightness_variation * 2.
+
+    shaped_noise[img_mask, :] = skimage.exposure.match_histograms(shaped_noise[img_mask, :] ** 1., contrast_adjusted_np_src[ref_mask, :], channel_axis=1)
+    shaped_noise = _np_src_image * (1. - np_mask_rgb) + shaped_noise * np_mask_rgb
+
+    matched_noise = shaped_noise.copy()
 
     return np.clip(matched_noise, 0., 1.)
+
 
 
 
