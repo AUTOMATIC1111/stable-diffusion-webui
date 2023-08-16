85f10df183f7b68fa6ba08880261915dd51e249f
wangdongming <wangdongming@hifive.ai>
2023-08-16 09:34:00 +0800

    [feat](user): 新增登录tss后端操作

--- modules/user.py
+++ modules/user.py
@@ -10,6 +10,8 @@ import os
 import time
 import typing
 
+import requests
+from modules.shared import opts
 from tools.mysql import get_mysql_cli
 
 
@@ -21,10 +23,13 @@ def authorization(user, password):
     if user in auths and auths.get(user) == password:
         return 3600 * 24 + int(time.time())
 
-    return find_users_from_models(user, password)
+    if os.getenv("ENABLE_TSS", "0") == '1':
+        return request_tu(user, password)
+
+    return find_users_from_db(user, password)
 
 
-def find_users_from_models(username, password) -> int:
+def find_users_from_db(username, password) -> int:
     host = os.getenv('MysqlHost')
     if host:
         cli = get_mysql_cli()
@@ -44,3 +49,30 @@ def find_users_from_models(username, password) -> int:
         finally:
             cli.close()
     return -1
+
+
+def request_tu(username, password):
+    host = 'https://draw-plus-backend-qa.xingzheai.cn'
+    path = "/v1/login"
+
+    try:
+        resp = requests.post(host+path, json={
+            'account': username,
+            'password': password
+        }, timeout=10)
+        if resp:
+            data = resp.json()
+            if data.get('code', -1) == 200:
+                token = data['token']
+                expire = time.localtime() + 10 * 3600
+                setattr(opts, 'tu-token', {
+                    'token': token,
+                    'expire': expire,
+                })
+                return expire
+
+    except:
+        return -1
+
+    return -1
+
