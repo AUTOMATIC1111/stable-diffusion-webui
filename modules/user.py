a19b2dd954b1b8304d1a8e00b3ac732cac5e1e70
wangdongming <wangdongming@hifive.ai>
2023-08-17 17:30:17 +0800

    [feat](*): 新增image表写入，支持脚本模型输入

--- modules/user.py
+++ modules/user.py
@@ -8,10 +8,15 @@
 import json
 import os
 import time
-import typing
+import base64
+import os
+import traceback
 
 import requests
+from hashlib import md5
 from modules.shared import opts
+from Crypto.Cipher import AES
+from Crypto import Random
 from tools.mysql import get_mysql_cli
 
 
@@ -51,28 +56,58 @@ def find_users_from_db(username, password) -> int:
     return -1
 
 
+def pad(data):
+    length = 16 - (len(data) % 16)
+    return (data + (chr(length) * length)).encode()
+
+
+def unpad(data):
+    return data[:-(data[-1] if type(data[-1]) == int else ord(data[-1]))]
+
+
+def bytes_to_key(data: bytes, salt: bytes, output: int = 48):
+    assert len(salt) == 8, len(salt)
+    data += salt
+    key = md5(data).digest()
+    final_key = key
+    while len(final_key) < output:
+        key = md5(key + data).digest()
+        final_key += key
+    return final_key[:output]
+
+
+def encrypt(message: str, passphrase: str):
+    salt = Random.new().read(8)
+    key_iv = bytes_to_key(passphrase.encode(), salt, 32+16)
+    key = key_iv[:32]
+    iv = key_iv[32:]
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    return base64.b64encode(b"Salted__" + salt + aes.encrypt(pad(message)))
+
+
 def request_tu(username, password):
     host = 'https://draw-plus-backend-qa.xingzheai.cn'
     path = "/v1/login"
 
     try:
-        resp = requests.post(host+path, json={
+        encrypted = encrypt(password, 'ZSYL20200707ZSYL')
+        resp = requests.post(host + path, json={
             'account': username,
-            'password': password
+            'password': encrypted.decode()
         }, timeout=10)
         if resp:
             data = resp.json()
             if data.get('code', -1) == 200:
-                token = data['token']
-                expire = time.localtime() + 10 * 3600
+                token = data['data']['token']
+                expire = time.time() + 10 * 3600
                 setattr(opts, 'tu-token', {
                     'token': token,
                     'expire': expire,
                 })
                 return expire
 
-    except:
+    except Exception as ex:
+        traceback.print_exc()
         return -1
 
     return -1
-
