18e3e6d6abfc084324cc8ae13f70ba4af5ddc35f
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-08-28 03:42:02 +0900

    consolidate local check

--- webui.py
+++ webui.py
@@ -74,7 +74,7 @@ def webui():
             if shared.opts.auto_launch_browser == "Remote" or cmd_opts.autolaunch:
                 auto_launch_browser = True
             elif shared.opts.auto_launch_browser == "Local":
-                auto_launch_browser = not any([cmd_opts.listen, cmd_opts.share, cmd_opts.ngrok, cmd_opts.server_name])
+                auto_launch_browser = not cmd_opts.webui_is_non_local
 
         app, local_url, share_url = shared.demo.launch(
             share=cmd_opts.share,
