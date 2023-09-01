e3a7a34e633e8686d25b830d42aecf223fa0c24a
osistlk <osistlk@gmail.com>
2023-09-01 13:37:25 -0400

    Python 3.11.5 compatiblity upgrade, tested locally

--- webui.sh
+++ webui.sh
@@ -128,12 +128,12 @@ case "$gpu_info" in
         if [[ -z "${TORCH_COMMAND}" ]]
         then
             pyv="$(${python_cmd} -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')"
-            if [[ $(bc <<< "$pyv <= 3.10") -eq 1 ]] 
+            if [[ $(bc <<< "$pyv <= 3.11") -eq 1 ]] 
             then
                 # Navi users will still use torch 1.13 because 2.0 does not seem to work.
                 export TORCH_COMMAND="pip install torch==1.13.1+rocm5.2 torchvision==0.14.1+rocm5.2 --index-url https://download.pytorch.org/whl/rocm5.2"
             else
-                printf "\e[1m\e[31mERROR: RX 5000 series GPUs must be using at max python 3.10, aborting...\e[0m"
+                printf "\e[1m\e[31mERROR: RX 5000 series GPUs must be using at max python 3.11, aborting...\e[0m"
                 exit 1
             fi
         fi
