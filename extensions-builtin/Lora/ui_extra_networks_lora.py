e785402b6acca12108e15224ff80d58817ab3c27
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-09-09 17:28:06 +0900

    return nothing if not found

--- extensions-builtin/Lora/ui_extra_networks_lora.py
+++ extensions-builtin/Lora/ui_extra_networks_lora.py
@@ -17,6 +17,8 @@ class ExtraNetworksPageLora(ui_extra_networks.ExtraNetworksPage):
 
     def create_item(self, name, index=None, enable_filter=True):
         lora_on_disk = networks.available_networks.get(name)
+        if lora_on_disk is None:
+            return
 
         path, ext = os.path.splitext(lora_on_disk.filename)
 
@@ -69,7 +71,6 @@ class ExtraNetworksPageLora(ui_extra_networks.ExtraNetworksPage):
         names = list(networks.available_networks)
         for index, name in enumerate(names):
             item = self.create_item(name, index)
-
             if item is not None:
                 yield item
 
--- modules/ui_extra_networks_textual_inversion.py
+++ modules/ui_extra_networks_textual_inversion.py
@@ -14,6 +14,8 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
 
     def create_item(self, name, index=None, enable_filter=True):
         embedding = sd_hijack.model_hijack.embedding_db.word_embeddings.get(name)
+        if embedding is None:
+            return
 
         path, ext = os.path.splitext(embedding.filename)
         return {
@@ -31,7 +33,9 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
     def list_items(self):
         names = list(sd_hijack.model_hijack.embedding_db.word_embeddings)
         for index, name in enumerate(names):
-            yield self.create_item(name, index)
+            item = self.create_item(name, index)
+            if item is not None:
+                yield item
 
     def allowed_directories_for_previews(self):
         return list(sd_hijack.model_hijack.embedding_db.embedding_dirs)
