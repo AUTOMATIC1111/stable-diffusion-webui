408ad841724bc50e56f27f0f0fb2338ca9c1fc9f
yinheli <me@yinheli.com>
2023-09-25 06:48:45 +0000

    fix sd_checkpoints_limit not works as expected

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -671,7 +671,7 @@ def reuse_model_from_already_loaded(sd_model, checkpoint_info, timer):
             already_loaded = loaded_model
             continue
 
-        if len(model_data.loaded_sd_models) > shared.opts.sd_checkpoints_limit > 0:
+        if len(model_data.loaded_sd_models) >= shared.opts.sd_checkpoints_limit > 0:
             print(f"Unloading model {len(model_data.loaded_sd_models)} over the limit of {shared.opts.sd_checkpoints_limit}: {loaded_model.sd_checkpoint_info.title}")
             model_data.loaded_sd_models.pop()
             send_model_to_trash(loaded_model)
