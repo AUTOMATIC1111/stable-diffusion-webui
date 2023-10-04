195c6628648b6a4a15ffc5b3c04fe7638449c7e4
Vladimir Mandic <mandic00@live.com>
2023-10-04 14:53:00 -0400

    parametrize gen-styles

--- cli/gen-styles.py
+++ cli/gen-styles.py
@@ -1,17 +1,13 @@
 #!/bin/env python
 
 import io
-import sys
 import json
 import base64
+import argparse
 import requests
 from PIL import Image
 
 
-url = 'http://127.0.0.1:7860'
-size = 128
-quality = 35
-prompt = 'woman walking in a city'
 options = {
     "negative_prompt": "",
     "steps": 20,
@@ -28,7 +24,7 @@ options = {
 styles = []
 
 
-def pil_to_b64(img: Image):
+def pil_to_b64(img: Image, size: int, quality: int):
     img = img.convert('RGB')
     img = img.resize((size, size))
     buffer = io.BytesIO()
@@ -38,7 +34,7 @@ def pil_to_b64(img: Image):
 
 
 def post(endpoint: str, dct: dict = None):
-    req = requests.post(f'{url}{endpoint}', json = dct, timeout=300, verify=False)
+    req = requests.post(endpoint, json = dct, timeout=300, verify=False)
     if req.status_code != 200:
         return { 'error': req.status_code, 'reason': req.reason, 'url': req.url }
     else:
@@ -46,25 +42,38 @@ def post(endpoint: str, dct: dict = None):
 
 
 if __name__ == '__main__':
-    sys.argv.pop(0)
-    if len(sys.argv) < 2:
-        print('gen-styles.py <input text file> <output json file>')
-        sys.exit(1)
-    with open(sys.argv[0], encoding='utf-8') as f:
+    parser = argparse.ArgumentParser(description = 'gen-styles.py')
+    parser.add_argument('--input', type=str, required=True, help="input text file with one line per prompt")
+    parser.add_argument('--output', type=str, required=True, help="output json file")
+    parser.add_argument('--nopreviews', default=False, action='store_true', help = 'generate previews')
+    parser.add_argument('--prompt', type=str, required=False, default='girl walking in a city', help="applied prompt when generating previews")
+    parser.add_argument('--size', type=int, default=128, help="image size for previews")
+    parser.add_argument('--quality', type=int, default=35, help="image quality for previews")
+    parser.add_argument('--url', type=str, required=False, default='http://127.0.0.1:7860', help="sd.next server url")
+    args = parser.parse_args()
+    with open(args.input, encoding='utf-8') as f:
         lines = f.readlines()
     for line in lines:
         line = line.strip().replace('\n', '')
-        print(line)
-        options['prompt'] = f'{line} {prompt}'
-        data = post('/sdapi/v1/txt2img', options)
-        b64str = data['images'][0].split(',',1)[0]
-        image = Image.open(io.BytesIO(base64.b64decode(b64str)))
+        if len(line) == 0:
+            continue
+        print(f'processing: {line}')
+        if not args.nopreviews:
+            options['prompt'] = f'{line} {args.prompt}'
+            data = post(f'{args.url}/sdapi/v1/txt2img', options)
+            if 'error' in data:
+                print(f'error: {data}')
+                continue
+            b64str = data['images'][0].split(',',1)[0]
+            image = Image.open(io.BytesIO(base64.b64decode(b64str)))
+        else:
+            image = None
         styles.append({
             'name': line,
             'prompt': line + ' {prompt}',
             'negative': '',
             'extra': '',
-            'preview': pil_to_b64(image)
+            'preview': pil_to_b64(image, args.size, args.quality) if image is not None else '',
         })
-        with open(sys.argv[1], 'w', encoding='utf-8') as outfile:
+        with open(args.output, 'w', encoding='utf-8') as outfile:
             json.dump(styles, outfile, indent=2)
