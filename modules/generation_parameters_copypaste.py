498d89d26ab428f751566bebe2bac846325d0452
liubo0902 <38622806+liubo0902@users.noreply.github.com>
2023-10-09 10:36:37 +0800

    get prompt from shared.infotext_params

--- modules/generation_parameters_copypaste.py
+++ modules/generation_parameters_copypaste.py
@@ -364,10 +364,7 @@ def create_override_settings_dict(text_pairs):
 def connect_paste(button, paste_fields, input_comp, override_settings_component, tabname):
     def paste_func(prompt):
         if not prompt and not shared.cmd_opts.hide_ui_dir_config:
-            filename = os.path.join(data_path, "params.txt")
-            if os.path.exists(filename):
-                with open(filename, "r", encoding="utf8") as file:
-                    prompt = file.read()
+            prompt = shared.infotext_params
 
         params = parse_generation_parameters(prompt)
         script_callbacks.infotext_pasted_callback(prompt, params)
