361a67e6399a25499f49b71240406426dc4b1dc3
liubo0902 <38622806+liubo0902@users.noreply.github.com>
2023-10-09 10:33:18 +0800

    add infotext_params in shared

--- modules/shared.py
+++ modules/shared.py
@@ -85,3 +85,5 @@ list_checkpoint_tiles = shared_items.list_checkpoint_tiles
 refresh_checkpoints = shared_items.refresh_checkpoints
 list_samplers = shared_items.list_samplers
 reload_hypernetworks = shared_items.reload_hypernetworks
+
+infotext_params = ''
