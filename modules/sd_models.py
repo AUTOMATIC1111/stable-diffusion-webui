5484edc4a3c8981be1b5c8f0c36d7da612b7018b
Won-Kyu Park <wkpark@gmail.com>
2023-10-11 05:12:29 +0900

    call hijack, apply_unet for reusing model

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -713,6 +713,9 @@ def reuse_model_from_already_loaded(sd_model, checkpoint_info, timer):
             shared.opts.data["sd_checkpoint_hash"] = already_loaded.sd_checkpoint_info.sha256
 
         print(f"Using already loaded model {already_loaded.sd_checkpoint_info.title}: done in {timer.summary()}")
+        sd_hijack.model_hijack.hijack(already_loaded)
+        sd_unet.apply_unet()
+
         sd_vae.reload_vae_weights(already_loaded)
         return model_data.sd_model
     elif shared.opts.sd_checkpoints_limit > 1 and len(model_data.loaded_sd_models) < shared.opts.sd_checkpoints_limit:
