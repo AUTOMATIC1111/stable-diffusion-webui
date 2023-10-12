498c23824f27ed5826e891e8b39164e9c3709383
Hameer Abbasi <einstein.edison@gmail.com>
2023-10-12 14:40:19 +0200

    Revert tuple compoarison.

--- installer.py
+++ installer.py
@@ -405,10 +405,9 @@ def check_torch():
         except Exception as e:
             log.debug(f'ROCm hipconfig failed: {e}')
             rocm_ver = None
-        # ROCm 5.7 and above don't have PyTorch 2.1.0.
-        if rocm_ver_tuple[:2] <= (5, 6):
+        if rocm_ver_tuple[:2] == "5.7":
             # install torch nightly via torchvision to avoid wasting bandwidth when torchvision depends on torch from yesterday
-            torch_command = os.environ.get('TORCH_COMMAND', f'torchvision --pre --index-url https://download.pytorch.org/whl/nightly/rocm{rocm_ver}')
+            torch_command = os.environ.get('TORCH_COMMAND', 'torchvision --pre --index-url https://download.pytorch.org/whl/nightly/rocm{rocm_ver}')
         else:
             # ROCm 5.5 is oldest for PyTorch 2.1
             torch_command = os.environ.get('TORCH_COMMAND', f'torch torchvision --index-url https://download.pytorch.org/whl/rocm5.5')
