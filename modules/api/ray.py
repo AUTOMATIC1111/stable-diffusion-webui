a955bf20e26f4fca310dca771f254695d1b05f0f
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-10-13 17:04:43 -0400

    Update ray.py

--- modules/api/ray.py
+++ modules/api/ray.py
@@ -14,7 +14,11 @@ ray.init()
 def ray_only():
     serve.shutdown()
     #serve.start(port=int(os.environ.get("RAY_PORT", 8000)))
-    serve.start(detached=True, http_options={"host": "0.0.0.0"})
+    serve.start(detached=True, http_options={
+                                            "host": "0.0.0.0", 
+                                            "port": int(os.environ.get("RAY_PORT", 8000))
+                                            }
+                )
     print(f"Starting Raypi on port {os.environ.get('RAY_PORT', 8000)}")
     serve.run(Raypi.bind(), port=int(os.environ.get("RAY_PORT", 8000)), route_prefix="/sdapi/v1")  #route_prefix="/sdapi/v1" # Call the launch_ray method to get the FastAPI app
     print("Done setting up replicas! Now accepting requests...")
