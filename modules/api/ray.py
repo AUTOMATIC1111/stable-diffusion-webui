42aacab2fc4616b17ca8680ce18c507d527609ba
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-10-13 15:13:03 -0400

    fixing env var

--- modules/api/ray.py
+++ modules/api/ray.py
@@ -14,7 +14,7 @@ ray.init()
 def ray_only():
     serve.shutdown()
     serve.start()
-    serve.run(Raypi.bind(), port=int(os.environ.get("RAY_NUM_GPUS", 8000)), route_prefix="/sdapi/v1")  #route_prefix="/sdapi/v1" # Call the launch_ray method to get the FastAPI app
+    serve.run(Raypi.bind(), port=int(os.environ.get("RAY_PORT", 8000)), route_prefix="/sdapi/v1")  #route_prefix="/sdapi/v1" # Call the launch_ray method to get the FastAPI app
     print("Done setting up replicas! Now accepting requests...")
     while True:
         time.sleep(1000)
