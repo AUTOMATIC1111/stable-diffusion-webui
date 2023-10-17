ac19967bbcc592b170ab902650810d6d35dcf204
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-10-17 14:47:40 -0400

    minor cleanup

--- modules/api/ray.py
+++ modules/api/ray.py
@@ -19,12 +19,17 @@ def ray_only():
     if "RAY_DOCKER" in os.environ:
         print("starting ray in docker")
         serve.start(detached=True, http_options={
-                                                    "host": os.environ.get("RAY_IP", "0.0.0.0"), 
-                                                    "port": int(os.environ.get("RAY_PORT", 8000))
-                                                    }
-                        )
+                                                "host": os.environ.get("RAY_IP", "0.0.0.0"), 
+                                                "port": int(os.environ.get("RAY_PORT", 8000))
+                                                }
+        )
     else:    
-        serve.start()
+        serve.start(
+            http_options={
+                        "host": os.environ.get("RAY_IP", "0.0.0.0"), 
+                        "port": int(os.environ.get("RAY_PORT", 8000))
+                        }
+        )
     print(f"Starting Raypi on port {os.environ.get('RAY_PORT', 8000)}")
     serve.run(Raypi.bind(), port=int(os.environ.get("RAY_PORT", 8000)), route_prefix="/sdapi/v1")  #route_prefix="/sdapi/v1" # Call the launch_ray method to get the FastAPI app
     print("Done setting up replicas! Now accepting requests...")
