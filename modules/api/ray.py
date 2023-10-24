baac3a6c3c7dc9af740f26e43ac7ec91c00a7a3b
webcoderz <19884161+webcoderz@users.noreply.github.com>
2023-10-23 23:31:15 -0400

    fix default dashboard port

--- modules/api/ray.py
+++ modules/api/ray.py
@@ -13,8 +13,8 @@ import os
 
 if "RAY_DOCKER" in os.environ:
     ray.init(
-        dashboard_host=os.environ.get("RAY_DASHBOARD_HOST", "0.0.0.0"), 
-        dashboard_port=int(os.environ.get("RAY_DASHBOARD_PORT", 8000))
+        dashboard_host=os.environ.get("RAY_DASHBOARD_HOST", "0.0.0.0"),
+        dashboard_port=int(os.environ.get("RAY_DASHBOARD_PORT", 8265))
         )
 else:
     ray.init()
