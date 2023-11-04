b2af7e0d952a5877ed988bb68ec3115b166cce6c
Vladimir Mandic <mandic00@live.com>
2023-11-04 13:20:22 -0400

    lint updates

--- modules/ui_interrogate.py
+++ modules/ui_interrogate.py
@@ -65,11 +65,11 @@ def image_analysis(image, clip_model_name):
     top_movements = ci.movements.rank(image_features, 5)
     top_trendings = ci.trendings.rank(image_features, 5)
     top_flavors = ci.flavors.rank(image_features, 5)
-    medium_ranks = {medium: sim for medium, sim in zip(top_mediums, ci.similarities(image_features, top_mediums))}
-    artist_ranks = {artist: sim for artist, sim in zip(top_artists, ci.similarities(image_features, top_artists))}
-    movement_ranks = {movement: sim for movement, sim in zip(top_movements, ci.similarities(image_features, top_movements))}
-    trending_ranks = {trending: sim for trending, sim in zip(top_trendings, ci.similarities(image_features, top_trendings))}
-    flavor_ranks = {flavor: sim for flavor, sim in zip(top_flavors, ci.similarities(image_features, top_flavors))}
+    medium_ranks = dict(zip(top_mediums, ci.similarities(image_features, top_mediums)))
+    artist_ranks = dict(zip(top_artists, ci.similarities(image_features, top_artists)))
+    movement_ranks = dict(zip(top_movements, ci.similarities(image_features, top_movements)))
+    trending_ranks = dict(zip(top_trendings, ci.similarities(image_features, top_trendings)))
+    flavor_ranks = dict(zip(top_flavors, ci.similarities(image_features, top_flavors)))
     return medium_ranks, artist_ranks, movement_ranks, trending_ranks, flavor_ranks
 
 
