7f3d3ca9720aaa6c15cc1936216c64e0fe2e038b
Charlie Joynt <cjj1977@users.noreply.github.com>
2023-11-06 08:39:33 +0000

    insert per-file dividers in styles list

--- modules/styles.py
+++ modules/styles.py
@@ -31,12 +31,15 @@ def apply_styles_to_prompt(prompt, styles):
     return prompt
 
 
-re_spaces = re.compile("  +")
+re_spaces = re.compile("\s{2,}+")
+re_commas = re.compile("(, )+")
 
 
 def extract_style_text_from_prompt(style_text, prompt):
     stripped_prompt = re.sub(re_spaces, " ", prompt.strip())
+    stripped_prompt = re.sub(re_commas, ", ", prompt.strip(","))
     stripped_style_text = re.sub(re_spaces, " ", style_text.strip())
+    stripped_style_text = re.sub(re_commas, ", ", style_text.strip(","))
     if "{prompt}" in stripped_style_text:
         left, right = stripped_style_text.split("{prompt}", 2)
         if stripped_prompt.startswith(left) and stripped_prompt.endswith(right):
@@ -101,7 +104,7 @@ class StyleDatabase:
             for file in os.listdir(path):
                 if fnmatch.fnmatch(file, fileglob):
                     self.styles[file.upper()] = PromptStyle(
-                        f"### {file.upper()}", None, None, None
+                        f"{file.upper()}", None, None, "divider"
                     )
                     self.load_from_csv(os.path.join(path, file))
         elif not os.path.exists(self.path):
@@ -142,27 +145,37 @@ class StyleDatabase:
         )
 
     def save_styles(self, path: str = None) -> None:
+        # The path argument is deprecated, but kept for backwards compatibility
+        _ = path
+
         # Update any styles without a path to the default path
-        for style in (s for s in self.styles.values() if not s.path):
-            self.styles[style.name] = style._replace(path=self.default_path)
+        for style in list(self.styles.values()):
+            if not style.path:
+                self.styles[style.name] = style._replace(path=self.default_path)
 
         # Create a list of all distinct paths, including the default path
         style_paths = set()
         style_paths.add(self.default_path)
-        for key, style in self.styles.items():
-            style_paths.add(style.path)
+        for _, style in self.styles.items():
+            if style.path:
+                style_paths.add(style.path)
+
+        csv_names = [os.path.split(path)[1].lower() for path in style_paths]
 
         for style_path in style_paths:
             # Always keep a backup file around
             if os.path.exists(style_path):
                 shutil.copy(style_path, f"{style_path}.bak")
 
+            # Write the styles to the CSV file
             with open(style_path, "w", encoding="utf-8-sig", newline="") as file:
                 writer = csv.DictWriter(file, fieldnames=self.prompt_fields)
                 writer.writeheader()
                 for style in (s for s in self.styles.values() if s.path == style_path):
-                    if style.name.lower() in style_path.lower():
+                    # Skip style list dividers, e.g. "STYLES.CSV"
+                    if style.name.lower().strip("# ") in csv_names:
                         continue
+                    # Write style fields, ignoring the path field
                     writer.writerow(
                         {k: v for k, v in style._asdict().items() if k != "path"}
                     )
