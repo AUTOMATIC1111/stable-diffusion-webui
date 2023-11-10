5432d9301359945b595d5e6649c7a64b4bb0bfca
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-11-11 03:38:55 +0900

    fix added accordion settings options

--- modules/ui.py
+++ modules/ui.py
@@ -4,6 +4,7 @@ import os
 import sys
 from functools import reduce
 import warnings
+from contextlib import suppress
 
 import gradio as gr
 import gradio.utils
@@ -270,9 +271,7 @@ def create_ui():
         extra_tabs.__enter__()
 
         with gr.Tab("Generation", id="txt2img_generation") as txt2img_generation_tab, ResizeHandleRow(equal_height=False):
-            with gr.Accordion("Open for Settings", open=False), gr.Column(variant='compact', elem_id="txt2img_settings") \
-                if shared.opts.img2img_settings_accordion else gr.Column(variant='compact', elem_id="txt2img_settings"):
-
+            with gr.Accordion("Open for Settings", open=False) if shared.opts.txt2img_settings_accordion else suppress(), gr.Column(variant='compact', elem_id="txt2img_settings"):
                 scripts.scripts_txt2img.prepare_ui()
 
                 for category in ordered_ui_categories():
@@ -491,8 +490,7 @@ def create_ui():
         extra_tabs.__enter__()
 
         with gr.Tab("Generation", id="img2img_generation") as img2img_generation_tab, ResizeHandleRow(equal_height=False):
-            with gr.Accordion("Open for Settings", open=False), gr.Column(variant='compact', elem_id="img2img_settings") \
-                if shared.opts.img2img_settings_accordion else gr.Column(variant='compact', elem_id="img2img_settings"):
+            with gr.Accordion("Open for Settings", open=False) if shared.opts.img2img_settings_accordion else suppress(), gr.Column(variant='compact', elem_id="img2img_settings"):
 
                 copy_image_buttons = []
                 copy_image_destinations = {}
