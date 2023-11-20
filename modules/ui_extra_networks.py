314ae1535ea172fcdb0f5b3b2eecc5d4ce9112b5
Tom Haelbich <haelbito@outlook.com>
2023-11-20 16:19:54 +0100

    added option for default behavior of dir buttons

--- modules/ui_extra_networks.py
+++ modules/ui_extra_networks.py
@@ -138,8 +138,13 @@ class ExtraNetworksPage:
                         continue
 
                     subdir = os.path.abspath(x)[len(parentdir):].replace("\\", "/")
-                    if not subdir.startswith("/"):
-                        subdir = "/" + subdir
+
+                    if shared.opts.extra_networks_dir_button_function:
+                        if not subdir.startswith("/"):
+                            subdir = "/" + subdir
+                    else:
+                        while subdir.startswith("/"):
+                            subdir = subdir[1:]
 
                     is_empty = len(os.listdir(x)) == 0
                     if not is_empty and not subdir.endswith("/"):
