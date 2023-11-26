a15dd151ffb4d11556028b34561058bc44930427
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-11-26 21:55:50 +0900

    json.dump(ensure_ascii=False)
    improve json readability

--- modules/options.py
+++ modules/options.py
@@ -158,7 +158,7 @@ class Options:
         assert not cmd_opts.freeze_settings, "saving settings is disabled"
 
         with open(filename, "w", encoding="utf8") as file:
-            json.dump(self.data, file, indent=4)
+            json.dump(self.data, file, indent=4, ensure_ascii=False)
 
     def same_type(self, x, y):
         if x is None or y is None:
