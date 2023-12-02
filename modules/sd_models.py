78acdcf677a96894651ff0d7d8287f2a994f3781
drhead <runemaste644@gmail.com>
2023-12-02 14:09:18 -0500

    fix variable

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -374,7 +374,7 @@ def load_model_weights(model, checkpoint_info: CheckpointInfo, state_dict, timer
 
     if shared.cmd_opts.no_half:
         model.float()
-        model.alphas_cumprod_original = alphas_cumprod
+        model.alphas_cumprod_original = model.alphas_cumprod
         devices.dtype_unet = torch.float32
         timer.record("apply float()")
     else:
