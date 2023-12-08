9e1fff334c2428bc2b23ae9a09ee9461e988a265
Muhammad Rehan Aslam <19831661+ranareehanaslam@users.noreply.github.com>
2023-12-08 23:07:22 +0500

    Update webui.py
    IPv6 only

--- webui.py
+++ webui.py
@@ -39,11 +39,10 @@ def api_only():
 
     print(f"Startup time: {startup_timer.summary()}.")
     api.launch(
-    server_name="[::]" if cmd_opts.ipv6 else ("0.0.0.0" if cmd_opts.listen else "127.0.0.1"),
-    port=cmd_opts.port if cmd_opts.port else 7861,
-    root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else ""
-)
-
+        server_name="::" if cmd_opts.listen else "::",
+        port=cmd_opts.port if cmd_opts.port else 7861,
+        root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else ""
+    )
 
 
 def webui():
