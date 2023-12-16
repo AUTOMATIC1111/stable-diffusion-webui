98c5fa92015837706adfd9975d5f345ab74f1c99
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-12-16 22:14:39 +0900

    fix extras caption BLIP
    #14328

--- scripts/postprocessing_caption.py
+++ scripts/postprocessing_caption.py
@@ -25,6 +25,6 @@ class ScriptPostprocessingCeption(scripts_postprocessing.ScriptPostprocessing):
             captions.append(deepbooru.model.tag(pp.image))
 
         if "BLIP" in option:
-            captions.append(shared.interrogator.generate_caption(pp.image))
+            captions.append(shared.interrogator.interrogate(pp.image.convert("RGB")))
 
         pp.caption = ", ".join([x for x in captions if x])
