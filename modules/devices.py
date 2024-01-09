ca671e5d7b9d03227f01e6bcb350032b6d14e722
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-01-09 23:30:55 +0800

    rearrange if-statements for cpu

--- modules/devices.py
+++ modules/devices.py
@@ -185,14 +185,14 @@ def autocast(disable=False):
     if fp8 and device==cpu:
         return torch.autocast("cpu", dtype=torch.bfloat16, enabled=True)
 
-    if has_xpu() or has_mps() or cuda_no_autocast():
-        return manual_cast(dtype)
-
     if fp8 and dtype_inference == torch.float32:
         return manual_cast(dtype)
 
     if dtype == torch.float32 or dtype_inference == torch.float32:
         return contextlib.nullcontext()
+
+    if has_xpu() or has_mps() or cuda_no_autocast():
+        return manual_cast(dtype)
 
     return torch.autocast("cuda")
 
