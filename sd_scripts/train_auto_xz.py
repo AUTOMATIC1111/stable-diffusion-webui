84bc2af4fe8b862531677240032715ebb83906a4
Jackstrawcd <296601946@qq.com>
2024-01-10 11:10:18 +0800

    [feat](train_auto_xz): skin retouching换用不使用onnx模型版本

--- sd_scripts/train_auto_xz.py
+++ sd_scripts/train_auto_xz.py
@@ -931,8 +931,7 @@ def train_auto(
 
     body_list, head_list = custom_configurable_image_processing(
         train_data_dir, options, head_width, head_height, if_res_oribody=True, model_p=general_model_path)
-    skin_retouching = pipeline("skin-retouching-torch", model="damo/cv_unet_skin_retouching_torch",
-                               model_revision="v1.0.2")
+    skin_retouching = pipeline(Tasks.skin_retouching, model="damo/cv_unet_skin-retouching")
     portrait_enhancement = pipeline(Tasks.image_portrait_enhancement, model="damo/cv_gpen_image-portrait-enhancement",
                                     model_revision="v1.0.0")
 
