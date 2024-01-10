f0c4693ae8db376d4e64a5197f709ccf5d100d01
Jackstrawcd <296601946@qq.com>
2024-01-10 19:27:40 +0800

    [debug](transformers_pretrained): 添加打印

--- sd_scripts/library/transformers_pretrained.py
+++ sd_scripts/library/transformers_pretrained.py
@@ -1,6 +1,9 @@
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
 
-
+print(f"save PreTrainedTokenizerBase.from_pretrained: {PreTrainedTokenizerBase.from_pretrained}")
 # 记录原本的from_pretrained语义
 ori_tokenizer_from_pretrained = PreTrainedTokenizerBase.from_pretrained.__func__
 ori_model_from_pretrained = PreTrainedModel.from_pretrained.__func__
+
+
+
