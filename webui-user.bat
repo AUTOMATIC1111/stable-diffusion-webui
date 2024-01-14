c35db9946410248558314f7299035d8a99a3a9b3
Jonel865 <154910612+Jonel865@users.noreply.github.com>
2024-01-14 08:36:31 +0800

    update
    optimal operation

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --lowvram --precision full --no-half --skip-torch-cuda-test
+set COMMANDLINE_ARGS= --lowvram --precision full  --no-half
 
 call webui.bat
