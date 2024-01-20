25e8273d2f6481deca221b29d35093f6d0c9da6a
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-01-21 02:21:36 +0900

    re-work multi --styles-file
    --styles-file change to append str
    --styles-file is [] then defaults to [styles.csv]
    
    --styles-file accepts paths or paths with wildcard "*"
    
    the first `--styles-file` entry is use as the default styles file path
    if filename a wildcard then the first matching file is used
    if no match is found, create a new "styles.csv" in the same dir as the first path
    
    when saving a new style it will be save in the default styles file
    when saving a existing style, it will be saved to file it belongs to
    
    order of the styles files in the styles dropdown can be controlled to a certain degree by the order of --styles-file

--- modules/cmd_args.py
+++ modules/cmd_args.py
@@ -88,7 +88,7 @@ parser.add_argument("--gradio-img2img-tool", type=str, help='does not do anythin
 parser.add_argument("--gradio-inpaint-tool", type=str, help="does not do anything")
 parser.add_argument("--gradio-allowed-path", action='append', help="add path to gradio's allowed_paths, make it possible to serve files from it", default=[data_path])
 parser.add_argument("--opt-channelslast", action='store_true', help="change memory type for stable diffusion to channels last")
-parser.add_argument("--styles-file", type=str, help="filename to use for styles", default=os.path.join(data_path, 'styles.csv'))
+parser.add_argument("--styles-file", type=str, action='append', help="path or wildcard path of styles files, allow multiple entries.", default=[])
 parser.add_argument("--autolaunch", action='store_true', help="open the webui URL in the system's default browser upon launch", default=False)
 parser.add_argument("--theme", type=str, help="launches the UI with light or dark theme", default=None)
 parser.add_argument("--use-textbox-seed", action='store_true', help="use textbox for seeds in UI (no up/down, but possible to input long seeds)", default=False)
