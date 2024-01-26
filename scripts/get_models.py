aaef3b95e5a25683aac5d087e768b111ae18f7c0
Vu Le <leducvuvietnam@gmail.com>
2024-01-26 08:08:34 +0000

    changed to general model downloader

--- scripts/get_models.py
+++ scripts/get_models.py
@@ -1,28 +1,58 @@
 import os
 import gdown
 import requests
+from tqdm import tqdm
 
 STABLE_DIFFUSION_DIR = "models/Stable-diffusion/"
 CONTROL_NET_MODEL_DIR = "extensions/sd-webui-controlnet/models/"
 LORA_MODEL_DIR = "models/Lora/"
 
+
+def download_model(download_url, des_folder, filename):
+    "download_url can be a google drive file ID or a https link"
+    filepath = os.path.join(des_folder, filename)
+    if not os.path.exists(filepath):
+        if "https" not in download_url:
+            print(
+                f"Download model {filename} from google drive {download_url} to {des_folder}"
+            )
+            gdown.download(id=download_url, output=des_folder, quiet=False)
+        else:
+            print(
+                f"Download model {filename} from {download_url} to {des_folder}"
+            )
+            response = requests.get(download_url, stream=True)
+            total_size = int(response.headers.get("content-length", 0))
+            block_size = 1024  # 1 Kb
+            with open(filepath, "wb") as file, tqdm(
+                total=total_size, unit="iB", unit_scale=True, unit_divisor=1024
+            ) as bar:
+                for data in response.iter_content(block_size):
+                    bar.update(len(data))
+                    file.write(data)
+            print("\nDownload complete.")
+
+
 revAnimated_v122EOL_url = "1GGVM1a3jSQdGLszOZRi-aJkP2Kwq5opd"
-if not os.path.exists(os.path.join(STABLE_DIFFUSION_DIR, "revAnimated_v122EOL.safetensors")):
-    print("Downloading revAnimated_v122EOL.safetensors from 1GGVM1a3jSQdGLszOZRi-aJkP2Kwq5opd")
-    gdown.download(id=revAnimated_v122EOL_url, output=STABLE_DIFFUSION_DIR, quiet=False)
+download_model(
+    revAnimated_v122EOL_url,
+    STABLE_DIFFUSION_DIR,
+    "revAnimated_v122EOL.safetensors",
+)
 
 blindbox_v1_mix_url = "1NTBN4b4gq2zl8cM1ulcSkEvAa4sXsoth"
-if not os.path.exists(os.path.join(LORA_MODEL_DIR, "blindbox_v1_mix.safetensors")):
-    print(f"Downloading blindbox_v1_mix.safetensors from {blindbox_v1_mix_url}")
-    gdown.download(id=blindbox_v1_mix_url, output=LORA_MODEL_DIR, quiet=False)
-
+download_model(
+    blindbox_v1_mix_url, LORA_MODEL_DIR, "blindbox_v1_mix.safetensors"
+)
 
 ip_adapter_faceid_plus_sd15_url = "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plus_sd15.bin"
+download_model(
+    ip_adapter_faceid_plus_sd15_url,
+    CONTROL_NET_MODEL_DIR,
+    "ip-adapter-faceid-plus_sd15.bin",
+)
 
-ip_adapter_faceid_plus_sd15_file = os.path.join(CONTROL_NET_MODEL_DIR, os.path.basename(ip_adapter_faceid_plus_sd15_url))
-if not os.path.exists(ip_adapter_faceid_plus_sd15_file):
-    print("downloading ip_adapter_faceid_plus_sd15")
-    response = requests.get(ip_adapter_faceid_plus_sd15_url)
-    with open(ip_adapter_faceid_plus_sd15_file, 'wb') as file:
-        file.write(response.content)
-        print("downloaded ip_adapter_faceid_plus_sd15")
+dreamshaper_7_url = "1uRIdWeD3mi_FQ6u3-o0EsPtIgWSk2qAF"
+download_model(
+    dreamshaper_7_url, STABLE_DIFFUSION_DIR, "dreamshaper_7.safetensors"
+)
