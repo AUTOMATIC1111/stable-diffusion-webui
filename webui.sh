db4632f4ba8d8023f536e1d4a69a398b7b1c3d7c
Alex He <heye_dev@163.com>
2024-02-02 13:48:42 +0800

    Update to ROCm5.7 and PyTorch
    The webui.sh installs ROCm5.4.2 as default. The webui run failed with AMD
    Radeon Pro W7900 with **Segmentation Fault** at Ubuntu22.04 maybe the ABI
    compatibility issue.
    
    ROCm5.7 is the latest version supported by PyTorch (https://pytorch.org/)
    at now. I test it with AMD Radeon Pro W7900 by PyTorch+ROCm5.7 with PASS.
    
    Signed-off-by: Alex He <heye_dev@163.com>

--- webui.sh
+++ webui.sh
@@ -158,7 +158,7 @@ if ! echo "$gpu_info" | grep -q "NVIDIA";
 then
     if echo "$gpu_info" | grep -q "AMD" && [[ -z "${TORCH_COMMAND}" ]]
     then
-        export TORCH_COMMAND="pip install torch==2.0.1+rocm5.4.2 torchvision==0.15.2+rocm5.4.2 --index-url https://download.pytorch.org/whl/rocm5.4.2"
+	export TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7"
     fi
 fi
 
