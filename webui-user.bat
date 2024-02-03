dda16282faee0c318b8bd37d40c1e0b9642e42ce
Jonel865 <154910612+Jonel865@users.noreply.github.com>
2024-02-03 16:38:36 +0800

    up

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,5 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --lowvram --precision full  --no-half  --skip-torch-cuda-test  --disable-nan-check
-
+set COMMANDLINE_ARGS= --lowvram --precision full  --no-half  
 call webui.bat
