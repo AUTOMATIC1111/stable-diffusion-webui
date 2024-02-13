08946355fa4b24a102b194450fa9f930f7c8e772
Ameya <ameyagawde326@gmail.com>
2024-02-14 03:44:02 +0530

    set cmd args to default

--- webui-user.bat
+++ webui-user.bat
@@ -3,5 +3,4 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--api --nowebui --xformers --no-hashing --no-download-sd-model --timeout-keep-alive 100 --skip-python-version-check --skip-version-check --skip-torch-cuda-test --skip-install --disable-safe-unpickle
-call webui.bat
+set COMMANDLINE_ARGS=
