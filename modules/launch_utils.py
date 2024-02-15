18ec22bffea77158777c3a3225e632e25eb33138
RedDeltas <160131179+RedDeltas@users.noreply.github.com>
2024-02-15 12:26:14 +0000

    Added core.filemode=false so doesn't track changes in file permissions in more restrictive environments

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -188,7 +188,7 @@ def git_clone(url, dir, name, commithash=None):
         return
 
     try:
-        run(f'"{git}" clone "{url}" "{dir}"', f"Cloning {name} into {dir}...", f"Couldn't clone {name}", live=True)
+        run(f'"{git}" clone --config core.filemode=false "{url}" "{dir}"', f"Cloning {name} into {dir}...", f"Couldn't clone {name}", live=True)
     except RuntimeError:
         shutil.rmtree(dir, ignore_errors=True)
         raise
