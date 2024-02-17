cb52279c3e7cf145cc1b284b6a05c883e6995c9f
AUTOMATIC1111 <16777216c@gmail.com>
2024-02-17 21:30:21 +0300

    Merge pull request #14947 from AUTOMATIC1111/open-button
    option "open image button" open the actual dir

--- modules/ui_tempdir.py
+++ modules/ui_tempdir.py
@@ -81,3 +81,18 @@ def cleanup_tmpdr():
 
             filename = os.path.join(root, name)
             os.remove(filename)
+
+
+def is_gradio_temp_path(path):
+    """
+    Check if the path is a temp dir used by gradio
+    """
+    path = Path(path)
+    if shared.opts.temp_dir and path.is_relative_to(shared.opts.temp_dir):
+        return True
+    if gradio_temp_dir := os.environ.get("GRADIO_TEMP_DIR"):
+        if path.is_relative_to(gradio_temp_dir):
+            return True
+    if path.is_relative_to(Path(tempfile.gettempdir()) / "gradio"):
+        return True
+    return False
