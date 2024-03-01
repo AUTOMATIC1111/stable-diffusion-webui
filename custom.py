1c6221929706a73e290e31026b0d19d22702b0f6
monsterhunters <88552297+monsterhunters@users.noreply.github.com>
2024-03-02 00:50:08 +0630

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,31 @@
+import os
+
+def installAnimateDiff():
+      os.system('git clone https://github.com/continue-revolution/sd-webui-animatediff.git')
+
+def installtext2video():
+      os.system('git clone https://github.com/kabachuha/sd-webui-text2video')
+
+def installReactor():
+      os.system('git clone https://github.com/Gourieff/sd-webui-reactor')
+
+def installDeforum():
+       os.system('git clone https://github.com/deforum-art/sd-webui-deforum.git')
+
+def highRes():
+       os.system('git clone https://github.com/klimaleksus/stable-diffusion-webui-conditioning-highres-fix.git')
+
+def twoShot():
+       os.system('git clone https://github.com/opparco/stable-diffusion-webui-two-shot.git')
+
+def composableLora():
+       os.system('git clone https://github.com/opparco/stable-diffusion-webui-composable-lora.git')
+
+def locon():
+       os.system('git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-locon.git')
+
+def llul():
+       os.system('git clone https://github.com/hnmr293/sd-webui-llul.git')
+
+def ControlNet():
+       os.system('git clone https://github.com/lllyasviel/ControlNet.git')
