2d57a2df660ec096969c89eb1ec72ebaa9a34636
Andray <33491867+light-and-ray@users.noreply.github.com>
2024-03-11 07:40:15 +0400

    Update modules/shared.py
    Co-authored-by: catboxanon <122327233+catboxanon@users.noreply.github.com>

--- modules/shared.py
+++ modules/shared.py
@@ -6,9 +6,9 @@ import gradio as gr
 from modules import shared_cmd_options, shared_gradio_themes, options, shared_items, sd_models_types
 from modules.paths_internal import models_path, script_path, data_path, sd_configs_path, sd_default_config, sd_model_file, default_sd_model_file, extensions_dir, extensions_builtin_dir  # noqa: F401
 from modules import util
+from typing import TYPE_CHECKING
 
-falseVar = False # avoid circular import for type hinting
-if falseVar:
+if TYPE_CHECKING:
     from modules import shared_state, styles, interrogate, shared_total_tqdm, memmon
 
 cmd_opts = shared_cmd_options.cmd_opts
