06c5dd0907ab15a1ef608b73ec0541fc258712c8
Andray <light.and.ray@gmail.com>
2024-03-17 14:28:26 +0400

    maybe fix tests

--- modules/postprocessing.py
+++ modules/postprocessing.py
@@ -133,13 +133,14 @@ def run_postprocessing_webui(id_task, *args, **kwargs):
     return run_postprocessing(*args, **kwargs)
 
 
-def run_extras(extras_mode, resize_mode, image, image_folder, input_dir, output_dir, show_extras_results, gfpgan_visibility, codeformer_visibility, codeformer_weight, upscaling_resize, upscaling_resize_w, upscaling_resize_h, upscaling_crop, extras_upscaler_1, extras_upscaler_2, extras_upscaler_2_visibility, upscale_first: bool, save_output: bool = True):
+def run_extras(extras_mode, resize_mode, image, image_folder, input_dir, output_dir, show_extras_results, gfpgan_visibility, codeformer_visibility, codeformer_weight, upscaling_resize, upscaling_resize_w, upscaling_resize_h, upscaling_crop, extras_upscaler_1, extras_upscaler_2, extras_upscaler_2_visibility, upscale_first: bool, save_output: bool = True, limit_target_resolution = 0):
     """old handler for API"""
 
     args = scripts.scripts_postproc.create_args_for_run({
         "Upscale": {
             "upscale_mode": resize_mode,
             "upscale_by": upscaling_resize,
+            "limit_target_resolution": limit_target_resolution,
             "upscale_to_width": upscaling_resize_w,
             "upscale_to_height": upscaling_resize_h,
             "upscale_crop": upscaling_crop,
