61f488302f37ad959eb5bb2eebed7e106c40c8b7
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2024-03-20 13:28:32 +0700

    FIX: Allow PNG-RGBA for Extras Tab

--- modules/ui_postprocessing.py
+++ modules/ui_postprocessing.py
@@ -12,7 +12,7 @@ def create_ui():
         with gr.Column(variant='compact'):
             with gr.Tabs(elem_id="mode_extras"):
                 with gr.TabItem('Single Image', id="single_image", elem_id="extras_single_tab") as tab_single:
-                    extras_image = gr.Image(label="Source", source="upload", interactive=True, type="pil", elem_id="extras_image")
+                    extras_image = gr.Image(label="Source", source="upload", interactive=True, type="pil", elem_id="extras_image", image_mode="RGBA")
 
                 with gr.TabItem('Batch Process', id="batch_process", elem_id="extras_batch_process_tab") as tab_batch:
                     image_batch = gr.Files(label="Batch Process", interactive=True, elem_id="extras_image_batch")
