5c5594ff1632ccab6c1f2ae7de3f6c69b1ab6f78
AUTOMATIC1111 <16777216c@gmail.com>
2024-03-21 07:09:40 +0300

    linter

--- modules/initialize_util.py
+++ modules/initialize_util.py
@@ -29,7 +29,7 @@ def fix_pytorch_lightning():
     if 'pytorch_lightning.utilities.distributed' not in sys.modules:
         import pytorch_lightning
         # Lets the user know that the library was not found and then will set it to pytorch_lightning.utilities.rank_zero
-        print(f"Pytorch_lightning.distributed not found, attempting pytorch_lightning.rank_zero")
+        print("Pytorch_lightning.distributed not found, attempting pytorch_lightning.rank_zero")
         sys.modules["pytorch_lightning.utilities.distributed"] = pytorch_lightning.utilities.rank_zero
 
 def fix_asyncio_event_loop_policy():
