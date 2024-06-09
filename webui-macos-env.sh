5e96ffc334db2a28b713e094a68e6408828f2641
Darrel Pol <poldarrel.dev@gmail.com>
2024-06-09 20:59:54 +0800

    adding extensions

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -9,7 +9,7 @@ then
     python_cmd="python3.10"
 fi
 
-export install_dir="$HOME"
+# export install_dir="$HOME"
 export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
 export TORCH_COMMAND="pip install torch==2.1.0 torchvision==0.16.0"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
