27a1812a1ae2b1f0e2a0a6d8eb769236be917bce
Panchovix <panchovixmenn@gmail.com>
2024-06-18 12:21:45 -0400

    Fix AttributeError: 'ImageDraw' object has no attribute 'multiline_textsize' for Pillow >=10 when making grids.
    Fixes AttributeError: 'ImageDraw' object has no attribute 'multiline_textsize' when using Pillow >10 trying to do X/Y/Z grids. This specially happens if using Python 3.12, where Pillow 10.1.0 is the least compatible version.
    
    Draft for now, since stable is using Python 3.10. It can be applied either if updating the Pillow library version in the requirements or if using Python 3.12.

--- modules/images.py
+++ modules/images.py
@@ -168,9 +168,12 @@ def draw_grid_annotations(im, width, height, hor_texts, ver_texts, margin=0):
         for line in lines:
             fnt = initial_fnt
             fontsize = initial_fontsize
-            while drawing.multiline_textsize(line.text, font=fnt)[0] > line.allowed_width and fontsize > 0:
+            text_width, text_height = drawing.multiline_textbbox((0, 0), line.text, font=fnt)[2:]
+            while text_width > line.allowed_width and fontsize > 0:
                 fontsize -= 1
-                fnt = get_font(fontsize)
+                fnt = get_font(fontsize) 
+                text_width, text_height = drawing.multiline_textbbox((0, 0), line.text, font=fnt)[2:]
+
             drawing.multiline_text((draw_x, draw_y + line.size[1] / 2), line.text, font=fnt, fill=color_active if line.is_active else color_inactive, anchor="mm", align="center")
 
             if not line.is_active:
