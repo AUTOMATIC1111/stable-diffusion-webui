ec3c31e7a19f3240bfba072787399eb02b88dc9e
viking1304 <viking1304@gmail.com>
2024-06-25 21:01:33 +0200

    Try to use specified python version on linux and mac, with fallback

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -4,11 +4,6 @@
 # Please modify webui-user.sh to change these instead of this file #
 ####################################################################
 
-if [[ -x "$(command -v python3.10)" ]]
-then
-    python_cmd="python3.10"
-fi
-
 export install_dir="$HOME"
 export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
