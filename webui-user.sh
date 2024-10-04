a40397ed6b6cd7c8a236b680d3247100aee54380
m0nd0CH1CK3N <m0nd0CH1CK3N@gmail.com>
2024-10-04 01:33:34 -0700

    setup

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--autolaunch --theme dark --xformers --medvram --no-half --no-download-sd-model --update-check --update-all-extensions --api"
 
 # python3 executable
 #python_cmd="python3"
