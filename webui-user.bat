c96a05475e3bed42c2f191f13a5cb3d023a912d8
dugmai <139248805+dugmai@users.noreply.github.com>
2024-10-17 21:24:34 +0700

    test

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
+
 @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers  --api --autolaunch --opt-split-attention --no-half-vae --theme dark
 
 call webui.bat
