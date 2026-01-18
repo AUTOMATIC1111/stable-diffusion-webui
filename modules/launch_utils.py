9ef4f62d69a017f2a7b18e4ef9994f5423b9896e
Sevketcan <sevketcankalyoncu@hotmail.com>
2026-01-19 01:07:58 +0300

    feat: Add `launch_utils.py` to centralize web UI launch, Git, and installation logic, and update `webui-user.bat`.

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -317,7 +317,7 @@ def requirements_met(requirements_file):
 
 def prepare_environment():
     torch_index_url = os.environ.get('TORCH_INDEX_URL', "https://download.pytorch.org/whl/cu121")
-    torch_command = os.environ.get('TORCH_COMMAND', f"pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url {torch_index_url}")
+    torch_command = os.environ.get('TORCH_COMMAND', f"pip install torch==2.9.1 torchvision==0.24.1 --extra-index-url {torch_index_url}")
     if args.use_ipex:
         if platform.system() == "Windows":
             # The "Nuullll/intel-extension-for-pytorch" wheels were built from IPEX source for Intel Arc GPU: https://github.com/intel/intel-extension-for-pytorch/tree/xpu-main
--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--skip-python-version-check --skip-torch-cuda-test --skip-install
+set STABLE_DIFFUSION_REPO=https://github.com/CompVis/stable-diffusion.git
+set STABLE_DIFFUSION_COMMIT_HASH=main
 
 call webui.bat
